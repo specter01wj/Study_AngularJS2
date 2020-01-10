427640273d12523352be219ded5f812274870a65
specter01wj <specter01wj@gmail.com>
2020-01-10 12:34:46 -0500

    01/06/2020 v1

--- Docs/Getting_Started/my-first-app/src/app/app-routing.module.ts
+++ Docs/Getting_Started/my-first-app/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ProductListComponent } from './product-list/product-list.component';
+
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+    ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
