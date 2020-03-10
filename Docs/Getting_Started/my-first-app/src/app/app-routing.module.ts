879732503f463ba18eefc910bb673e5e5eafd1db
specter01wj <specter01wj@gmail.com>
2020-03-10 13:22:40 -0400

    03/10/2020 v1

--- Docs/Getting_Started/my-first-app/src/app/app-routing.module.ts
+++ Docs/Getting_Started/my-first-app/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDashboardComponent } from './product-dashboard/product-dashboard.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot([
       { path: '', component: ProductDashboardComponent },
-      { path: 'product-list', component: ProductListComponent }
+      { path: 'product-list', component: ProductListComponent },
+      { path: 'product-list/:productId', component: ProductDetailsComponent }
     ])],
   exports: [RouterModule]
 })
