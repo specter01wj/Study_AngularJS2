001dfd4099a87bcd4fdb123e485465d5b0c584df
specter01wj <specter01wj@gmail.com>
2020-03-10 20:29:46 -0400

    03/10/2020 v1

--- Docs/Getting_Started/my-first-app/src/app/service/cart.service.ts
+++ Docs/Getting_Started/my-first-app/src/app/service/cart.service.ts
@@ -15,7 +15,24 @@ export class CartService {
   }
 
   getItems() {
-    return this.items;
+    // return this.items;
+    return this.items = [
+	  {
+	    name: 'Phone XL',
+	    price: 799,
+	    description: 'A large phone with one of the best screens'
+	  },
+	  {
+	    name: 'Phone Mini',
+	    price: 699,
+	    description: 'A great phone with one of the best cameras'
+	  },
+	  {
+	    name: 'Phone Standard',
+	    price: 299,
+	    description: ''
+	  }
+	];
   }
 
   clearCart() {
