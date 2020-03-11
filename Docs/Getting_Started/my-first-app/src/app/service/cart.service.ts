e0f0cfc2f7168f64d26253cb5375038533fe0f80
specter01wj <specter01wj@gmail.com>
2020-03-10 20:47:00 -0400

    03/10/2020 v1

--- Docs/Getting_Started/my-first-app/src/app/service/cart.service.ts
+++ Docs/Getting_Started/my-first-app/src/app/service/cart.service.ts
@@ -9,7 +9,7 @@ export class CartService {
 
   items = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
 
   addToCart(product) {
@@ -40,6 +40,10 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
   }
 
 }
