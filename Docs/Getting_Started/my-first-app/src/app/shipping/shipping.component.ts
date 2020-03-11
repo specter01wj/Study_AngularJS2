858ca92ffe432d37efe107f22913c8289836f673
specter01wj <specter01wj@gmail.com>
2020-03-11 03:02:08 -0400

    03/06/2020 v1

--- Docs/Getting_Started/my-first-app/src/app/shipping/shipping.component.ts
+++ Docs/Getting_Started/my-first-app/src/app/shipping/shipping.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartService } from '../service/cart.service';
+
 @Component({
   selector: 'app-shipping',
   templateUrl: './shipping.component.html',
   styleUrls: ['./shipping.component.less']
 })
 export class ShippingComponent implements OnInit {
+  
+  shippingCosts;
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit() {
+  	this.shippingCosts = this.cartService.getShippingPrices();
   }
 
 }
