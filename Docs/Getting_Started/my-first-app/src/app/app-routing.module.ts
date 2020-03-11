d7894dd8d63a25ef5a60814d8eae84ee2bf64709
specter01wj <specter01wj@gmail.com>
2020-03-11 03:05:16 -0400

    03/06/2020 v1

--- Docs/Getting_Started/my-first-app/src/app/app-routing.module.ts
+++ Docs/Getting_Started/my-first-app/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDashboardComponent } from './product-dashboard/product-dashboard.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 const routes: Routes = [];
 
@@ -13,7 +14,8 @@ const routes: Routes = [];
       { path: '', component: ProductDashboardComponent },
       { path: 'product-list', component: ProductListComponent },
       { path: 'product-list/:productId', component: ProductDetailsComponent },
-      { path: 'cart', component: CartComponent }
+      { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent }
     ])],
   exports: [RouterModule]
 })
