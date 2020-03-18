5c093d93a8ab2fb2b310f8719785b1f5cbd46a55
specter01wj <specter01wj@gmail.com>
2020-03-18 12:29:45 -0400

    03/18/2020 v1

--- Docs/FUNDAMENTALS/2.Tour of Heroes App/mysol_1/tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ Docs/FUNDAMENTALS/2.Tour of Heroes App/mysol_1/tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Hero } from '../interface/hero';
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
