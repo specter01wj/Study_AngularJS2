d523083db3439ab2a98c348b8f396884b02415a5
specter01wj <specter01wj@gmail.com>
2020-03-18 19:20:54 -0400

    03/18/2020 v1

--- Docs/FUNDAMENTALS/2.Tour of Heroes App/mysol_1/tour-of-heroes/src/app/service/hero.service.ts
+++ Docs/FUNDAMENTALS/2.Tour of Heroes App/mysol_1/tour-of-heroes/src/app/service/hero.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
 
+import { Hero } from '../interface/hero';
+import { HEROES } from '../api/mock-heroes';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
   constructor() { }
+
+  getHeroes(): Hero[] {
+	return HEROES;
+  }
+
 }
