3debf2fc3fbe069e6f72934767bb9018e1adfbbe
specter01wj <specter01wj@gmail.com>
2020-03-22 22:06:48 -0600

    03/18/2020 v1

--- Docs/FUNDAMENTALS/2.Tour of Heroes App/mysol_1/tour-of-heroes/src/app/app-routing.module.ts
+++ Docs/FUNDAMENTALS/2.Tour of Heroes App/mysol_1/tour-of-heroes/src/app/app-routing.module.ts
@@ -5,7 +5,9 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 
 const routes: Routes = [
-	{ path: 'heroes', component: HeroesComponent }
+	{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+	{ path: 'heroes', component: HeroesComponent },
+	{ path: 'dashboard', component: DashboardComponent }
 ];
 
 @NgModule({
