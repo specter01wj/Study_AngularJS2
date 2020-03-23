5cbbef5186d03d62167569053ad9a679ba59137c
specter01wj <specter01wj@gmail.com>
2020-03-22 22:19:11 -0600

    03/18/2020 v1

--- Docs/FUNDAMENTALS/2.Tour of Heroes App/mysol_1/tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ Docs/FUNDAMENTALS/2.Tour of Heroes App/mysol_1/tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 import { Hero } from '../interface/hero';
+import { HeroService }  from '../service/hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -10,7 +13,11 @@ import { Hero } from '../interface/hero';
 export class HeroDetailComponent implements OnInit {
   @Input("hero-in") hero: Hero;
 
-  constructor() { }
+  constructor(
+  	private route: ActivatedRoute,
+	  private heroService: HeroService,
+	  private location: Location
+  ) { }
 
   ngOnInit() {
   }
--- Docs/FUNDAMENTALS/2.Tour of Heroes App/mysol_1/tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Docs/FUNDAMENTALS/2.Tour of Heroes App/mysol_1/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -21,21 +21,21 @@ export class HeroesComponent implements OnInit {
 
   	// heroes = HEROES;
   	heroes: Hero[];
-  	selectedHero: Hero;
+  	// selectedHero: Hero;
 
   	constructor(
-  		private heroService: HeroService,
-  		private messageService: MessageService
+  		private heroService: HeroService
+  		// private messageService: MessageService
   	) { }
 
 	ngOnInit() {
 		this.getHeroes();
 	}
 
-	onSelect(hero: Hero): void {
+	/*onSelect(hero: Hero): void {
 		this.selectedHero = hero;
 		this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
-	}
+	}*/
 
 	/*getHeroes(): void {
 		this.heroes = this.heroService.getHeroes();
