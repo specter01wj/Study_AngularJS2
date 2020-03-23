a51c90ce5e2ee1db5218f60c802369e7a9612c00
specter01wj <specter01wj@gmail.com>
2020-03-22 22:02:53 -0600

    03/18/2020 v1

--- Docs/FUNDAMENTALS/2.Tour of Heroes App/mysol_1/tour-of-heroes/src/app/dashboard/dashboard.component.ts
+++ Docs/FUNDAMENTALS/2.Tour of Heroes App/mysol_1/tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Hero } from '../interface/hero';
+import { HeroService } from '../service/hero.service';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.less']
 })
 export class DashboardComponent implements OnInit {
+	heroes: Hero[] = [];
 
   constructor() { }
 
