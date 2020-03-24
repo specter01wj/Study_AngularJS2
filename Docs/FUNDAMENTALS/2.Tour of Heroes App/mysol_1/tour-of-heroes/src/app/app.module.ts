622faea364bfa53a3fabb1a394a3de0d80bd789f
Jin Wang <specter01wj@gmail.com>
2020-03-23 23:20:02 -0600

    03/23/2020 v1

--- Docs/FUNDAMENTALS/2.Tour of Heroes App/mysol_1/tour-of-heroes/src/app/app.module.ts
+++ Docs/FUNDAMENTALS/2.Tour of Heroes App/mysol_1/tour-of-heroes/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +25,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
