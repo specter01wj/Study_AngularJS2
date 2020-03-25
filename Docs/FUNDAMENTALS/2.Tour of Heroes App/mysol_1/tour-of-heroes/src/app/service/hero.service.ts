a95d1bdbfca153f5a6f78925db1bd8e1cb4125a4
Jin Wang <specter01wj@gmail.com>
2020-03-24 23:25:04 -0600

    03/24/2020 v1

--- Docs/FUNDAMENTALS/2.Tour of Heroes App/mysol_1/tour-of-heroes/src/app/service/hero.service.ts
+++ Docs/FUNDAMENTALS/2.Tour of Heroes App/mysol_1/tour-of-heroes/src/app/service/hero.service.ts
@@ -85,12 +85,13 @@ export class HeroService {
       // if not search term, return empty hero array.
       return of([]);
     }
-    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
-      tap(x => x.length ?
-         this.log(`found heroes matching "${term}"`) :
-         this.log(`no heroes matching "${term}"`)),
-      catchError(this.handleError<Hero[]>('searchHeroes', []))
-    );
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`)
+      .pipe(
+        tap(x => x.length ?
+           this.log(`found heroes matching "${term}"`) :
+           this.log(`no heroes matching "${term}"`)),
+        catchError(this.handleError<Hero[]>('searchHeroes', []))
+      );
   }
 
   /**
