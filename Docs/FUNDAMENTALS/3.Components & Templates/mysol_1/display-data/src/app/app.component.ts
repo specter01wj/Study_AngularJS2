7d235d3a0247eaed612997418df0164656881e0b
Jin Wang <specter01wj@gmail.com>
2020-03-25 21:35:40 -0600

    03/24/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/display-data/src/app/app.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/display-data/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'display-data';
+  title = 'Tour of Heroes';
+  myHero = 'Windstorm';
 }
