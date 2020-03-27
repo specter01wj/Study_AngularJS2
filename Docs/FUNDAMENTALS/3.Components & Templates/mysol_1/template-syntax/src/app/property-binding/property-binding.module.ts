04da3ad846ef66a7cf57f095571740f4c35cbfa9
specter01wj <specter01wj@gmail.com>
2020-03-27 00:16:06 -0600

    03/18/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/property-binding/property-binding.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/property-binding/property-binding.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ItemDetailComponent } from './item-detail/item-detail.component';
 import { ItemListComponent } from './item-list/item-list.component';
 import { StringInitComponent } from './string-init/string-init.component';
+import { PropertyBindingComponent } from './property-binding.component';
 
 
 
 @NgModule({
-  declarations: [ItemDetailComponent, ItemListComponent, StringInitComponent],
+  declarations: [ItemDetailComponent, ItemListComponent, StringInitComponent, PropertyBindingComponent],
   imports: [
     CommonModule
   ]
--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/property-binding/property-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/property-binding/property-routing.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
+import { PropertyBindingComponent } from './property-binding.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { StringInitComponent } from './string-init/string-init.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: PropertyBindingComponent,
+    children: [
+      { path: 'item-detail', component: ItemDetailComponent },
+      { path: 'item-list', component: ItemListComponent },
+      { path: 'string-init', component: StringInitComponent },
+    ]
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forChild(routes)
   ]
 })
 export class PropertyRoutingModule { }
