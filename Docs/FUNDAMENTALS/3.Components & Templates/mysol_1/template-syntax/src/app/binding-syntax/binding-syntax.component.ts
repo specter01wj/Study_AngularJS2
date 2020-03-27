bf2ec7866be656bf54770c942b2478ce6ac39571
specter01wj <specter01wj@gmail.com>
2020-03-26 19:45:37 -0600

    03/18/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/binding-syntax/binding-syntax.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/binding-syntax/binding-syntax.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-binding-syntax',
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./binding-syntax.component.less']
 })
 export class BindingSyntaxComponent implements OnInit {
+	@ViewChild('bindingInput') bindingInput: ElementRef;
+
+  isUnchanged = true;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  getHTMLAttributeValue(): any {
+    console.warn('HTML attribute value: ' + this.bindingInput.nativeElement.getAttribute('value'));
+  }
+
+  getDOMPropertyValue(): any {
+    console.warn('DOM property value: ' + this.bindingInput.nativeElement.value);
+  }
+
+  working(): any {
+    console.warn('Test Button works!');
+  }
+
+  toggleDisabled(): any {
+
+    let testButton = <HTMLInputElement> document.getElementById('testButton');
+    testButton.disabled = !testButton.disabled;
+    console.warn(testButton.disabled);
   }
 
 }
