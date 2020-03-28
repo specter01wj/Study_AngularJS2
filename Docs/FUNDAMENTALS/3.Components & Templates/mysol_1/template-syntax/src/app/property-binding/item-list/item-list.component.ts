8cc5bb15b2e9f72b7743e336b5ada499dc24b557
specter01wj <specter01wj@gmail.com>
2020-03-27 22:20:02 -0600

    03/18/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/property-binding/item-list/item-list.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/property-binding/item-list/item-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { ITEMS } from '../mock-items';
+import { Item } from '../item';
 
 @Component({
   selector: 'app-item-list',
@@ -6,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-list.component.less']
 })
 export class ItemListComponent implements OnInit {
+	@Input() items: Item[];
+  listItems = ITEMS;
 
   constructor() { }
 
