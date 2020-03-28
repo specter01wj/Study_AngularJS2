2e94969e9f5837442d9360c9aa963fe450092968
specter01wj <specter01wj@gmail.com>
2020-03-27 22:52:59 -0600

    03/18/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/app.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/app.module.ts
@@ -1,23 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { TemplateSyntaxModule } from './template-syntax/template-syntax';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BindingSyntaxComponent } from './binding-syntax/binding-syntax.component';
-import { TemplateSyntaxComponent } from './template-syntax/template-syntax.component';
+// import { TemplateSyntaxComponent } from './template-syntax/template-syntax.component';
 import { HeroDetailComponent } from './template-syntax/hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BindingSyntaxComponent,
-    TemplateSyntaxComponent,
+    // TemplateSyntaxComponent,
     HeroDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TemplateSyntaxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/template-syntax/template-syntax.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+@NgModule({
+  declarations: [
+  	HeroDetailComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TemplateSyntaxModule { }
