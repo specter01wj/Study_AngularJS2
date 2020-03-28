a4b3b307f6360efd0addf7dfe5b530ff22724c59
specter01wj <specter01wj@gmail.com>
2020-03-27 22:21:35 -0600

    03/18/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/property-binding/string-init/string-init.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/property-binding/string-init/string-init.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-string-init',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./string-init.component.less']
 })
 export class StringInitComponent implements OnInit {
+	@Input() prefix: string;
 
   constructor() { }
 
