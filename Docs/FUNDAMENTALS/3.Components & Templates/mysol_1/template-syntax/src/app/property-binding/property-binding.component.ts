966e203b6fc5f0da95c28a310dab8a42dfb8be91
specter01wj <specter01wj@gmail.com>
2020-03-27 22:26:05 -0600

    03/18/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/property-binding/property-binding.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/property-binding/property-binding.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyBindingComponent implements OnInit {
 	title: string = "PropertyBinding";
+	itemImageUrl = 'assets/images/optimum.jpeg';
+  isUnchanged = true;
+  classes = 'special';
+  parentItem = 'lamp';
+
+  currentItems = [{
+    id: 21,
+    name: 'phone'
+  }];
+
+  interpolationTitle = 'Interpolation';
+  propertyTitle = 'Property binding';
+
+  evilTitle = 'Template <script>alert("evil never sleeps")</script> Syntax';
 
   constructor() { }
 
