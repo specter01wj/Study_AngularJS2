8a73ae0930fbefb2e0765fce1c4fd9dacbaa9602
specter01wj <specter01wj@gmail.com>
2020-03-27 22:18:49 -0600

    03/18/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/property-binding/item-detail/item-detail.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/property-binding/item-detail/item-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item-detail',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-detail.component.less']
 })
 export class ItemDetailComponent implements OnInit {
+	@Input() childItem: string;
+
+	currentItem = 'bananas in boxes';
 
   constructor() { }
 
