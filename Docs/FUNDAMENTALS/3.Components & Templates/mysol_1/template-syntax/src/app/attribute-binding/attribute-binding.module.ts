451b244d2b749e15e4253d360270f3700e02fe8f
specter01wj <specter01wj@gmail.com>
2020-03-31 20:38:17 -0600

    03/31/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/attribute-binding/attribute-binding.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/attribute-binding/attribute-binding.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AttributeBindingComponent } from './attribute-binding.component';
 
+import { AttributeRoutingModule } from './attribute-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  	AttributeBindingComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AttributeRoutingModule
   ]
 })
 export class AttributeBindingModule { }
--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/attribute-binding/attribute-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/attribute-binding/attribute-routing.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
+import { AttributeBindingComponent } from './attribute-binding.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: AttributeBindingComponent,
+    children: [
+      
+    ]
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class AttributeRoutingModule { }
