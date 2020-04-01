f823bfc4eb22d8d0c4ec91415d360800cfe5861b
specter01wj <specter01wj@gmail.com>
2020-03-31 21:51:51 -0600

    03/31/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/attribute-binding/attribute-binding.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/attribute-binding/attribute-binding.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AttributeBindingComponent } from './attribute-binding.component';
-
 import { AttributeRoutingModule } from './attribute-routing.module';
+
+import { AttributeBindingComponent } from './attribute-binding.component';
 import { CompWithHostBindingComponent } from './comp-with-host-binding/comp-with-host-binding.component';
 
 @NgModule({
