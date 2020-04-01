1507199d359e304cf53f00c3d7171e91f2c90be1
specter01wj <specter01wj@gmail.com>
2020-03-31 21:43:00 -0600

    03/31/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/attribute-binding/comp-with-host-binding/comp-with-host-binding.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/attribute-binding/comp-with-host-binding/comp-with-host-binding.component.ts
@@ -3,9 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-comp-with-host-binding',
   templateUrl: './comp-with-host-binding.component.html',
-  styleUrls: ['./comp-with-host-binding.component.less']
+  styleUrls: ['./comp-with-host-binding.component.less'],
+  host: {
+    '[class.special]': 'isSpecial',
+    '[style.color]': 'color',
+    '[style.width]': 'width'
+  }
 })
 export class CompWithHostBindingComponent implements OnInit {
+	isSpecial = false;
+  color = 'green';
+  width = '200px';
 
   constructor() { }
 
