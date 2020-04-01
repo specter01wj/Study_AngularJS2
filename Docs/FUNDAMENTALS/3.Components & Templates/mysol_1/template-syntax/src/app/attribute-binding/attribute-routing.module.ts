388ebd71c7a9168865241bb3b00f7aea0a64d721
specter01wj <specter01wj@gmail.com>
2020-03-31 21:00:31 -0600

    03/31/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/attribute-binding/attribute-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/attribute-binding/attribute-routing.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AttributeBindingComponent } from './attribute-binding.component';
+import { CompWithHostBindingComponent } from './comp-with-host-binding/comp-with-host-binding.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AttributeBindingComponent,
     children: [
-      
+      { path: 'comp-with-host-binding', component: CompWithHostBindingComponent },
     ]
   }
 ];
