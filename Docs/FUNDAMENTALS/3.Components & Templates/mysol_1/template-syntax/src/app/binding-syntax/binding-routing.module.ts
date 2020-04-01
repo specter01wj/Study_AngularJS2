fdbd40d79991c17badf1187f2c6f7c4a7fda67ab
specter01wj <specter01wj@gmail.com>
2020-03-31 21:06:47 -0600

    03/31/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/binding-syntax/binding-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/binding-syntax/binding-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
+import { BindingSyntaxComponent } from './binding-syntax.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: BindingSyntaxComponent,
+    children: [
+    ]
+  }
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  	BindingSyntaxComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class BindingRoutingModule { }
