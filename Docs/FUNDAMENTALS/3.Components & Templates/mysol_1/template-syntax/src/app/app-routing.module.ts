709503f4cee5e48b66288d1611095d18c75076a2
specter01wj <specter01wj@gmail.com>
2020-03-31 21:17:44 -0600

    03/31/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/app-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-// import { BindingSyntaxComponent } from './binding-syntax/binding-syntax.component';
-// import { TemplateSyntaxComponent } from './template-syntax/template-syntax.component';
-
 const routes: Routes = [
 	{ path: '', redirectTo: '/template-syntax', pathMatch: 'full' },
 	// { path: 'template-syntax', component: TemplateSyntaxComponent },
