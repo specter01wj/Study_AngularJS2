42a5f2a3ec0694a209114d75c1b6114213d4c293
specter01wj <specter01wj@gmail.com>
2020-03-31 21:51:28 -0600

    03/31/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/property-binding/property-binding.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/property-binding/property-binding.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PropertyRoutingModule } from './property-routing.module';
+
 import { ItemDetailComponent } from './item-detail/item-detail.component';
 import { ItemListComponent } from './item-list/item-list.component';
 import { StringInitComponent } from './string-init/string-init.component';
 import { PropertyBindingComponent } from './property-binding.component';
-
-import { PropertyRoutingModule } from './property-routing.module';
 
 @NgModule({
   declarations: [
