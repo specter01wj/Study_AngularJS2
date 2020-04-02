97b0859536a2a925cc53a8cddde5b852a41ba181
specter01wj <specter01wj@gmail.com>
2020-04-01 21:31:02 -0600

    04/01/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/event-binding/event-binding.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/event-binding/event-binding.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { EventRoutingModule } from './event-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    EventRoutingModule
   ]
 })
 export class EventBindingModule { }
