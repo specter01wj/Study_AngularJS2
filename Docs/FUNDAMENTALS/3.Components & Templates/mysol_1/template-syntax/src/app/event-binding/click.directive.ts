2c41c89080dda1332b09807ceb693ffb299f3ea7
specter01wj <specter01wj@gmail.com>
2020-04-01 22:22:54 -0600

    04/01/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/event-binding/click.directive.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/event-binding/click.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, Output } from '@angular/core';
 
 @Directive({
   selector: '[appClick]'
 })
 export class ClickDirective {
+	@Output('myClick') clicks = new EventEmitter<string>();
 
-  constructor() { }
+	toggle = false;
+
+  constructor(el: ElementRef) { 
+  	el.nativeElement
+      .addEventListener('click', (event: Event) => {
+        this.toggle = !this.toggle;
+        this.clicks.emit(this.toggle ? 'Click!' : '');
+      });
+  }
 
 }
