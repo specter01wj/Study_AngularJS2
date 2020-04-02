acf730f6512fc5d9e2fc88c0e4d273c2da36653c
specter01wj <specter01wj@gmail.com>
2020-04-01 18:20:21 -0600

    04/01/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/template-syntax/big-hero-detail/big-hero-detail.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/template-syntax/big-hero-detail/big-hero-detail.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
+
+import { Hero } from '../../interface/hero';
+
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-big-hero-detail',
   templateUrl: './big-hero-detail.component.html',
   styleUrls: ['./big-hero-detail.component.less']
 })
-export class BigHeroDetailComponent implements OnInit {
+export class BigHeroDetailComponent implements OnInit extends HeroDetailComponent {
+	@Input()  hero: Hero;
+  @Output() deleteRequest = new EventEmitter<Hero>();
 
-  constructor() { }
+  delete() {
+    this.deleteRequest.emit(this.hero);
+  }
+  // constructor() { }
 
   ngOnInit(): void {
   }
