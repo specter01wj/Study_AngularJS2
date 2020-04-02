4f12d4d1826b2b5a42e0a29cec7968c1e110f7fb
specter01wj <specter01wj@gmail.com>
2020-04-01 22:00:24 -0600

    04/01/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/event-binding/event-binding.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/event-binding/event-binding.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ItemEvent } from '../interface/item-event';
+
 @Component({
   selector: 'app-event-binding',
   templateUrl: './event-binding.component.html',
   styleUrls: ['./event-binding.component.less']
 })
 export class EventBindingComponent implements OnInit {
+	currentItem = { name: 'teapot'} ;
+  clickMessage = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSave(event?: KeyboardEvent) {
+    const evtMsg = event ? ' Event target is ' + (<HTMLElement>event.target).textContent : '';
+    alert('Saved.' + evtMsg);
+    if (event) { event.stopPropagation(); }
+  }
+
+  deleteItem(item: Item) {
+    alert(`Delete the ${item.name}.`);
+  }
+
+  onClickMe(event?: KeyboardEvent) {
+    const evtMsg = event ? ' Event target class is ' + (<HTMLElement>event.target).className  : '';
+    alert('Click me.' + evtMsg);
   }
 
 }
