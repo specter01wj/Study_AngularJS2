96d703e435732b1154dc5a616d3c993f5e62b440
specter01wj <specter01wj@gmail.com>
2020-04-01 22:29:45 -0600

    04/01/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/event-binding/item-detail/item-detail.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/event-binding/item-detail/item-detail.component.ts
@@ -9,14 +9,22 @@ import { ItemEvent } from '../../interface/item-event';
 })
 export class ItemDetailComponent implements OnInit {
 	@Input() item;
-  itemImageUrl = 'assets/teapot.svg';
+  itemImageUrl = 'assets/images/teapot.svg';
   lineThrough = '';
   displayNone = '';
   @Input() prefix = '';
+  // This component makes a request but it can't actually delete a hero.
+  @Output() deleteRequest = new EventEmitter<ItemEvent>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  delete() {
+    this.deleteRequest.emit(this.item);
+    this.displayNone = this.displayNone ? '' : 'none';
+    this.lineThrough = this.lineThrough ? '' : 'line-through';
   }
 
 }
