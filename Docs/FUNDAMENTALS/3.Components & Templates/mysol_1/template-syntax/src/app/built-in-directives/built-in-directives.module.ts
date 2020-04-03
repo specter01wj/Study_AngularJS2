0b067434b7d53809c7eab721175c011238edbacf
specter01wj <specter01wj@gmail.com>
2020-04-03 12:56:17 -0600

    04/03/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/built-in-directives/built-in-directives.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/built-in-directives/built-in-directives.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BuiltInDirectivesComponent } from './built-in-directives.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
 
 
 
 @NgModule({
-  declarations: [BuiltInDirectivesComponent],
+  declarations: [BuiltInDirectivesComponent, ItemDetailComponent],
   imports: [
     CommonModule
   ]
