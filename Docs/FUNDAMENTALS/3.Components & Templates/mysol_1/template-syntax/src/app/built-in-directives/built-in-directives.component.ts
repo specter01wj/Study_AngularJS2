0a8e36a2fe9789ac4899dc30e3cc78a7093996b2
specter01wj <specter01wj@gmail.com>
2020-04-03 12:55:50 -0600

    04/03/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/built-in-directives/built-in-directives.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-built-in-directives',
+  templateUrl: './built-in-directives.component.html',
+  styleUrls: ['./built-in-directives.component.less']
+})
+export class BuiltInDirectivesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/built-in-directives/built-in-directives.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/built-in-directives/built-in-directives.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BuiltInDirectivesComponent } from './built-in-directives.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [BuiltInDirectivesComponent],
   imports: [
     CommonModule
   ]
