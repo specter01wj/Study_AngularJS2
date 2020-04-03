5797e6c4e06809a1933699e0ba2176d05d890e1a
specter01wj <specter01wj@gmail.com>
2020-04-02 20:01:57 -0600

    04/02/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/two-way-binding/two-way-binding.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/two-way-binding/two-way-binding.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { TwoWayRoutingModule } from './two-way-routing.module';
+
 import { TwoWayBindingComponent } from './two-way-binding.component';
 import { SizerComponent } from './sizer/sizer.component';
 
@@ -8,7 +12,9 @@ import { SizerComponent } from './sizer/sizer.component';
 @NgModule({
   declarations: [TwoWayBindingComponent, SizerComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    TwoWayRoutingModule
   ]
 })
 export class TwoWayBindingModule { }
