cd2951936fef6d39b331d4de302417563c3273fb
specter01wj <specter01wj@gmail.com>
2020-04-03 12:57:54 -0600

    04/03/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/built-in-directives/built-in-directives.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/built-in-directives/built-in-directives.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { BuiltInDRoutingModule } from './built-in-d-routing.module';
+
 import { BuiltInDirectivesComponent } from './built-in-directives.component';
 import { ItemDetailComponent } from './item-detail/item-detail.component';
 
@@ -8,7 +12,9 @@ import { ItemDetailComponent } from './item-detail/item-detail.component';
 @NgModule({
   declarations: [BuiltInDirectivesComponent, ItemDetailComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    BuiltInDRoutingModule
   ]
 })
 export class BuiltInDirectivesModule { }
