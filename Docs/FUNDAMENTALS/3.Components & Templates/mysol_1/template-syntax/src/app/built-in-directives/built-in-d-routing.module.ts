cb0e300c1d41a2afc473526708284b0e6d403ccb
specter01wj <specter01wj@gmail.com>
2020-04-03 12:59:07 -0600

    04/03/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/built-in-directives/built-in-d-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/built-in-directives/built-in-d-routing.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
+import { BuiltInDirectivesComponent } from './built-in-directives.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: BuiltInDirectivesComponent,
+    children: [
+      { path: 'item-detail', component: ItemDetailComponent }
+    ]
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class BuiltInDRoutingModule { }
--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/built-in-directives/built-in-directives.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/built-in-directives/built-in-directives.module.ts
@@ -7,8 +7,6 @@ import { BuiltInDRoutingModule } from './built-in-d-routing.module';
 import { BuiltInDirectivesComponent } from './built-in-directives.component';
 import { ItemDetailComponent } from './item-detail/item-detail.component';
 
-
-
 @NgModule({
   declarations: [BuiltInDirectivesComponent, ItemDetailComponent],
   imports: [
