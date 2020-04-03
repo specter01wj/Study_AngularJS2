6e902eeb7fbfb303a15a0c0dd250cc71a67ee001
specter01wj <specter01wj@gmail.com>
2020-04-02 20:07:21 -0600

    04/02/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/two-way-binding/two-way-binding.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/two-way-binding/two-way-binding.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./two-way-binding.component.less']
 })
 export class TwoWayBindingComponent implements OnInit {
-
+	fontSizePx = 16;
+	
   constructor() { }
 
   ngOnInit(): void {
