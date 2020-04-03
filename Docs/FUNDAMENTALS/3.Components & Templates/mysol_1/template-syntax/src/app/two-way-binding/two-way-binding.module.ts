399dd68a61ed8b2f71c4e18eb4256b6b5d1c95e8
specter01wj <specter01wj@gmail.com>
2020-04-02 19:57:35 -0600

    04/02/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/two-way-binding/two-way-binding.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/two-way-binding/two-way-binding.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TwoWayBindingComponent } from './two-way-binding.component';
+import { SizerComponent } from './sizer/sizer.component';
 
 
 
 @NgModule({
-  declarations: [TwoWayBindingComponent],
+  declarations: [TwoWayBindingComponent, SizerComponent],
   imports: [
     CommonModule
   ]
