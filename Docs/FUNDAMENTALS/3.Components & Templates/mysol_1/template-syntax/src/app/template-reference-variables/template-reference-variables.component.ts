ecec32c90a0dd03da934acc518ebce5ea026790f
specter01wj <specter01wj@gmail.com>
2020-04-07 16:48:13 -0600

    04/03/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/template-reference-variables/template-reference-variables.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/template-reference-variables/template-reference-variables.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-reference-variables',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template-reference-variables.component.less']
 })
 export class TemplateReferenceVariablesComponent implements OnInit {
+	@ViewChild('itemForm') form: NgForm;
+
+  private _submitMessage = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  get submitMessage() {
+    return this._submitMessage;
+  }
+
+  onSubmit(form: NgForm) {
+    this._submitMessage = 'Submitted. Form value is ' + JSON.stringify(form.value);
+  }
+
+  callPhone(value: string) {
+    console.warn(`Calling ${value} ...`);
+  }
+
+  callFax(value: string) {
+    console.warn(`Faxing ${value} ...`);
   }
 
 }
