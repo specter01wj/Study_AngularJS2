5d6940ad9356049ddbfdf169cb38690d10e08b59
specter01wj <specter01wj@gmail.com>
2020-04-07 12:07:26 -0600

    04/03/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/template-syntax/template-syntax.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/template-syntax/template-syntax.module.ts
@@ -8,13 +8,15 @@ import { TemplateSyntaxComponent } from './template-syntax.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { BigHeroDetailComponent } from './big-hero-detail/big-hero-detail.component';
 import { SizerComponent } from './sizer/sizer.component';
+import { heroSwitchComponents } from './hero-switch.components';
 
 @NgModule({
   declarations: [
   	TemplateSyntaxComponent,
   	HeroDetailComponent,
   	BigHeroDetailComponent,
-  	SizerComponent
+  	SizerComponent,
+    heroSwitchComponents
   ],
   imports: [
     CommonModule,
