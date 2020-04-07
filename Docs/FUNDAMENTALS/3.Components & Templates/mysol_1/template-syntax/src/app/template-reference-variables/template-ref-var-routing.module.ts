b2606129c0414ba8a48bc3088078ff1714278eaa
specter01wj <specter01wj@gmail.com>
2020-04-07 16:44:15 -0600

    04/03/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/template-reference-variables/template-ref-var-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/template-reference-variables/template-ref-var-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
+import { TemplateReferenceVariablesComponent } from './template-reference-variables.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: TemplateReferenceVariablesComponent,
+    children: [
+    ]
+  }
+];
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class TemplateRefVarRoutingModule { }
