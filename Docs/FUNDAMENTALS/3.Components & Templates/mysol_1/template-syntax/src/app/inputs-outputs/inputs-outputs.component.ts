d02e15b3e1855b7a4a3b3625c0246b53281f80ea
specter01wj <specter01wj@gmail.com>
2020-04-07 22:03:59 -0600

    04/03/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/inputs-outputs/inputs-outputs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inputs-outputs',
+  templateUrl: './inputs-outputs.component.html',
+  styleUrls: ['./inputs-outputs.component.less']
+})
+export class InputsOutputsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/inputs-outputs/inputs-outputs.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/inputs-outputs/inputs-outputs.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InputsOutputsComponent } from './inputs-outputs.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [InputsOutputsComponent],
   imports: [
     CommonModule
   ]
