f5d4af5b8d95358d71f07090152f0bcb8ceabee1
specter01wj <specter01wj@gmail.com>
2020-04-07 22:35:21 -0600

    04/03/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/inputs-outputs/aliasing/aliasing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aliasing',
+  templateUrl: './aliasing.component.html',
+  styleUrls: ['./aliasing.component.less']
+})
+export class AliasingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/inputs-outputs/inputs-outputs.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/inputs-outputs/inputs-outputs.module.ts
@@ -5,11 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { InputsOutputsRoutingModule } from './inputs-outputs-routing.module';
 
 import { InputsOutputsComponent } from './inputs-outputs.component';
+import { AliasingComponent } from './aliasing/aliasing.component';
 
 
 
 @NgModule({
-  declarations: [InputsOutputsComponent],
+  declarations: [InputsOutputsComponent, AliasingComponent],
   imports: [
     CommonModule,
     FormsModule,
