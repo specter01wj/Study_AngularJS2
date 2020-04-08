2c1a956961f8b109179edcb9cc75ca3f76037d73
specter01wj <specter01wj@gmail.com>
2020-04-07 22:40:06 -0600

    04/03/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/inputs-outputs/inputs-outputs-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/inputs-outputs/inputs-outputs-routing.module.ts
@@ -5,13 +5,22 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { InputsOutputsComponent } from './inputs-outputs.component';
 
+import { AliasingComponent } from './aliasing/aliasing.component';
+import { InTheMetadataComponent } from './in-the-metadata/in-the-metadata.component';
+import { InputOutputComponent } from './input-output/input-output.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { ItemOutputComponent } from './item-output/item-output.component';
 
 const routes: Routes = [
   {
     path: '',
     component: InputsOutputsComponent,
     children: [
-      { path: 'sizer', component: SizerComponent }
+      { path: 'aliasing', component: AliasingComponent },
+      { path: 'in-the-metadata', component: InTheMetadataComponent },
+      { path: 'input-output', component: InputOutputComponent },
+      { path: 'item-detail', component: ItemDetailComponent },
+      { path: 'item-output', component: ItemOutputComponent },
     ]
   }
 ];
--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/inputs-outputs/inputs-outputs.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/inputs-outputs/inputs-outputs.module.ts
@@ -5,16 +5,22 @@ import { FormsModule } from '@angular/forms';
 import { InputsOutputsRoutingModule } from './inputs-outputs-routing.module';
 
 import { InputsOutputsComponent } from './inputs-outputs.component';
+
 import { AliasingComponent } from './aliasing/aliasing.component';
 import { InTheMetadataComponent } from './in-the-metadata/in-the-metadata.component';
 import { InputOutputComponent } from './input-output/input-output.component';
 import { ItemDetailComponent } from './item-detail/item-detail.component';
 import { ItemOutputComponent } from './item-output/item-output.component';
 
-
-
 @NgModule({
-  declarations: [InputsOutputsComponent, AliasingComponent, InTheMetadataComponent, InputOutputComponent, ItemDetailComponent, ItemOutputComponent],
+  declarations: [
+  	InputsOutputsComponent, 
+  	AliasingComponent, 
+  	InTheMetadataComponent, 
+  	InputOutputComponent, 
+  	ItemDetailComponent, 
+  	ItemOutputComponent
+  ],
   imports: [
     CommonModule,
     FormsModule,
