28c3986665277d8e2813aeb4529ef840dc7a28d9
specter01wj <specter01wj@gmail.com>
2020-04-07 22:46:07 -0600

    04/03/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/inputs-outputs/inputs-outputs.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/inputs-outputs/inputs-outputs.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inputs-outputs.component.less']
 })
 export class InputsOutputsComponent implements OnInit {
+	currentItem = 'Television';
+
+  lastChanceItem = 'Beanbag';
+  items = ['item1', 'item2', 'item3', 'item4'];
+  wishlist = ['Drone', 'Computer'];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addItem(newItem: string) {
+    this.items.push(newItem);
+  }
+
+
+  crossOffItem(item: string) {
+    console.warn(`Parent says: crossing off ${item}.`);
+  }
+
+  buyClearanceItem(item) {
+    console.warn(`Parent says: buying ${item}.`);
+  }
+
+  saveForLater(item) {
+    console.warn(`Parent says: saving ${item} for later.`);
+  }
+
+  addToWishList(wish: string) {
+    console.warn(`Parent says: adding ${this.currentItem} to your wishlist.`);
+    this.wishlist.push(wish);
+    console.warn(this.wishlist);
   }
 
 }
