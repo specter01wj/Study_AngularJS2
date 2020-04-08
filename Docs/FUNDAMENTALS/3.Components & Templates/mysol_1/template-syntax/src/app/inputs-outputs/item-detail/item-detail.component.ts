aeccff2310f9f883a9ebd10aa205e61022540941
specter01wj <specter01wj@gmail.com>
2020-04-07 22:50:49 -0600

    04/03/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/inputs-outputs/item-detail/item-detail.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/inputs-outputs/item-detail/item-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-item-detail',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-detail.component.less']
 })
 export class ItemDetailComponent implements OnInit {
+	@Input() item: string; // decorate the property with @Input()
 
   constructor() { }
 
