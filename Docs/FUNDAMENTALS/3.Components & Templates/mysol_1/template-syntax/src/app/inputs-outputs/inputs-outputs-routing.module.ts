79528ee8aa4db5815ba16a1d75a1ceb4bef4d041
specter01wj <specter01wj@gmail.com>
2020-04-07 22:34:42 -0600

    04/03/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/inputs-outputs/inputs-outputs-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/inputs-outputs/inputs-outputs-routing.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+import { InputsOutputsComponent } from './inputs-outputs.component';
 
 
+const routes: Routes = [
+  {
+    path: '',
+    component: InputsOutputsComponent,
+    children: [
+      { path: 'sizer', component: SizerComponent }
+    ]
+  }
+];
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class InputsOutputsRoutingModule { }
