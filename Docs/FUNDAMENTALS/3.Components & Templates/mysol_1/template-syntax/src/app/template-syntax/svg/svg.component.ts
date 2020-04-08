a9ce106e6ca1ced24e969639c586f1405565b13c
specter01wj <specter01wj@gmail.com>
2020-04-08 16:21:08 -0600

    04/03/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/template-syntax/svg/svg.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/template-syntax/svg/svg.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./svg.component.less']
 })
 export class SvgComponent implements OnInit {
+	fillColor = 'rgb(255, 0, 0)';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+
+  changeColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    this.fillColor = `rgb(${r}, ${g}, ${b})`;
   }
 
 }
