7a3f222a99489cb3ffc4e02776d91ddedab6fd3e
specter01wj <specter01wj@gmail.com>
2020-04-07 22:49:59 -0600

    04/03/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/inputs-outputs/input-output/input-output.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/inputs-outputs/input-output/input-output.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-input-output',
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-output.component.less']
 })
 export class InputOutputComponent implements OnInit {
+	@Input() item: string;
+  @Output() deleteRequest = new EventEmitter<string>();
+
+  lineThrough = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+
+  delete() {
+    console.warn('Child says: emiting item deleteRequest with', this.item);
+    this.deleteRequest.emit(this.item);
+    this.lineThrough = this.lineThrough ? '' : 'line-through';
   }
 
 }
