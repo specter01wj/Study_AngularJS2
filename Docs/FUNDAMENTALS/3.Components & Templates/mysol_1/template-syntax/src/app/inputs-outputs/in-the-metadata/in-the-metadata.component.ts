05986acaff479fc582f6405527fa43e468590291
specter01wj <specter01wj@gmail.com>
2020-04-07 22:49:06 -0600

    04/03/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/inputs-outputs/in-the-metadata/in-the-metadata.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/mysol_1/template-syntax/src/app/inputs-outputs/in-the-metadata/in-the-metadata.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-in-the-metadata',
   templateUrl: './in-the-metadata.component.html',
-  styleUrls: ['./in-the-metadata.component.less']
+  styleUrls: ['./in-the-metadata.component.less'],
+  inputs: ['clearanceItem'],
+  outputs: ['buyEvent']
 })
 export class InTheMetadataComponent implements OnInit {
+	buyEvent = new EventEmitter<string>();
+  clearanceItem: string;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  buyIt() {
+    console.warn('Child says: emiting buyEvent with', this.clearanceItem);
+    this.buyEvent.emit(this.clearanceItem);
   }
 
 }
