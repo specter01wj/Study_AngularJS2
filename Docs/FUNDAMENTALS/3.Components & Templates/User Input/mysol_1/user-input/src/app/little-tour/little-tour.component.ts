5ff5650075bcf90ab6bc33ec5b3b2adb12e1cd0a
specter01wj <specter01wj@gmail.com>
2020-04-09 16:10:35 -0600

    04/03/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/User Input/mysol_1/user-input/src/app/little-tour/little-tour.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/User Input/mysol_1/user-input/src/app/little-tour/little-tour.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./little-tour.component.less']
 })
 export class LittleTourComponent implements OnInit {
+	heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  addHero(newHero: string) {
+    if (newHero) {
+      this.heroes.push(newHero);
+    }
   }
 
 }
