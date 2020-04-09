a2c8ef8ae75488e4ffc03f0dc7b19307931ce920
specter01wj <specter01wj@gmail.com>
2020-04-09 15:58:55 -0600

    04/03/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/User Input/mysol_1/user-input/src/app/app.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/User Input/mysol_1/user-input/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClickMeComponent } from './click-me/click-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClickMeComponent
   ],
   imports: [
     BrowserModule,
---
+++ Docs/FUNDAMENTALS/3.Components & Templates/User Input/mysol_1/user-input/src/app/click-me/click-me.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-click-me',
+  templateUrl: './click-me.component.html',
+  styleUrls: ['./click-me.component.less']
+})
+export class ClickMeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
