b23611c2609c98f8ddcf8a50247cbcff8fa863aa
specter01wj <specter01wj@gmail.com>
2020-04-09 16:08:34 -0600

    04/03/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/User Input/mysol_1/user-input/src/app/click-me2/click-me2.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/User Input/mysol_1/user-input/src/app/click-me2/click-me2.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./click-me2.component.less']
 })
 export class ClickMe2Component implements OnInit {
+	clickMessage = '';
+  clicks = 1;
 
   constructor() { }
 
   ngOnInit() {
+  }
+  
+
+  onClickMe2(event: any) {
+    let evtMsg = event ? ' Event target is ' + event.target.tagName  : '';
+    this.clickMessage = (`Click #${this.clicks++}. ${evtMsg}`);
   }
 
 }
