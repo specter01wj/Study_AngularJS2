7c16fafef82f2917751d28d93bde1302b690b719
specter01wj <specter01wj@gmail.com>
2020-04-21 17:14:59 -0600

    04/21/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/peek-a-boo/peek-a-boo.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/peek-a-boo/peek-a-boo.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LoggerService } from '../services/logger.service';
+
 @Component({
   selector: 'app-peek-a-boo',
   templateUrl: './peek-a-boo.component.html',
   styleUrls: ['./peek-a-boo.component.less']
 })
 export class PeekABooComponent implements OnInit {
+	hasChild = false;
+  hookLog: string[];
 
-  constructor() { }
+  heroName = 'Windstorm';
+  private logger: LoggerService;
+
+  constructor(logger: LoggerService) { 
+  	this.logger = logger;
+    this.hookLog = logger.logs;
+  }
 
   ngOnInit() {
+  }
+
+  toggleChild() {
+    this.hasChild = !this.hasChild;
+    if (this.hasChild) {
+      this.heroName = 'Windstorm';
+      this.logger.clear(); // clear log on create
+    }
+    this.hookLog = this.logger.logs;
+    this.logger.tick();
+  }
+
+  updateHero() {
+    this.heroName += '!';
+    this.logger.tick();
   }
 
 }
