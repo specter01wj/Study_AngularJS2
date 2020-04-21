8216c2aceca1191eb156f1b3d05ce68715aa4004
specter01wj <specter01wj@gmail.com>
2020-04-21 17:21:51 -0600

    04/21/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/peek-a-boo/peek-a-boo-child/peek-a-boo-child.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/peek-a-boo/peek-a-boo-child/peek-a-boo-child.component.ts
@@ -1,15 +1,88 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, 
+	AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges } from '@angular/core';
+
+import { LoggerService } from '../../services/logger.service';
+
+let nextId = 1;
+
+export class PeekABoo implements OnInit {
+  constructor(private logger: LoggerService) { }
+
+  // implement OnInit's `ngOnInit` method
+  ngOnInit() { this.logIt(`OnInit`); }
+
+  logIt(msg: string) {
+    this.logger.log(`#${nextId++} ${msg}`);
+  }
+}
 
 @Component({
   selector: 'app-peek-a-boo-child',
   templateUrl: './peek-a-boo-child.component.html',
   styleUrls: ['./peek-a-boo-child.component.less']
 })
-export class PeekABooChildComponent implements OnInit {
+export class PeekABooChildComponent extends PeekABoo implements 
+						 OnChanges, OnInit, DoCheck,
+             AfterContentInit, AfterContentChecked,
+             AfterViewInit, AfterViewChecked,
+             OnDestroy {
 
-  constructor() { }
+  @Input()  name: string;
+
+  private verb = 'initialized';
+
+  constructor(logger: LoggerService) { 
+  	super(logger);
+
+    let is = this.name ? 'is' : 'is not';
+    this.logIt(`name ${is} known at construction`);
+  }
 
   ngOnInit() {
   }
 
+  // only called for/if there is an @input variable set by parent.
+  ngOnChanges(changes: SimpleChanges) {
+    let changesMsgs: string[] = [];
+    for (let propName in changes) {
+      if (propName === 'name') {
+        let name = changes['name'].currentValue;
+        changesMsgs.push(`name ${this.verb} to "${name}"`);
+      } else {
+        changesMsgs.push(propName + ' ' + this.verb);
+      }
+    }
+    this.logIt(`OnChanges: ${changesMsgs.join('; ')}`);
+    this.verb = 'changed'; // next time it will be a change
+  }
+
+  // Beware! Called frequently!
+  // Called in every change detection cycle anywhere on the page
+  ngDoCheck() { this.logIt(`DoCheck`); }
+
+  ngAfterContentInit() { this.logIt(`AfterContentInit`);  }
+
+  // Beware! Called frequently!
+  // Called in every change detection cycle anywhere on the page
+  ngAfterContentChecked() { this.logIt(`AfterContentChecked`); }
+
+  ngAfterViewInit() { this.logIt(`AfterViewInit`); }
+
+  // Beware! Called frequently!
+  // Called in every change detection cycle anywhere on the page
+  ngAfterViewChecked() { this.logIt(`AfterViewChecked`); }
+
+  ngOnDestroy() { this.logIt(`OnDestroy`); }
+
 }
+
+
+
