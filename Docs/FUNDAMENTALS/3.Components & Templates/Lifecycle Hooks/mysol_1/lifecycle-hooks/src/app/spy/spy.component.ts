abe6e571e390d6cac679542443ace1e173e6e08b
specter01wj <specter01wj@gmail.com>
2020-04-21 22:58:42 -0600

    04/21/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/spy/spy.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/spy/spy.component.ts
@@ -16,4 +16,21 @@ export class SpyComponent implements OnInit {
   ngOnInit() {
   }
 
+  addHero() {
+    if (this.newName.trim()) {
+      this.heroes.push(this.newName.trim());
+      this.newName = '';
+      this.logger.tick();
+    }
+  }
+  removeHero(hero: string) {
+    this.heroes.splice(this.heroes.indexOf(hero), 1);
+    this.logger.tick();
+  }
+  reset() {
+    this.logger.log('-- reset --');
+    this.heroes = [];
+    this.logger.tick();
+  }
+
 }
