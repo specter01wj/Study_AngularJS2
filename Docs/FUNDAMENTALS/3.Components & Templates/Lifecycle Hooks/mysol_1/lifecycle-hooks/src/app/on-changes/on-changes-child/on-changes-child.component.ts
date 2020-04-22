1540666a20e88f57a830ad3a5b282d57b26c5d29
specter01wj <specter01wj@gmail.com>
2020-04-22 14:22:58 -0600

    04/17/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/on-changes/on-changes-child/on-changes-child.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-on-changes-child',
+  templateUrl: './on-changes-child.component.html',
+  styleUrls: ['./on-changes-child.component.less']
+})
+export class OnChangesChildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/on-changes/on-changes.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/on-changes/on-changes.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OnChangesComponent } from './on-changes.component';
+import { OnChangesChildComponent } from './on-changes/on-changes-child.component';
 
 
 
 @NgModule({
-  declarations: [OnChangesComponent],
+  declarations: [OnChangesComponent, OnChangesChildComponent],
   imports: [
     CommonModule
   ]
