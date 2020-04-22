da16fa7ea96a1ed5610c55a9b14777d1b6668e7d
specter01wj <specter01wj@gmail.com>
2020-04-22 14:21:45 -0600

    04/17/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/on-changes/on-changes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-on-changes',
+  templateUrl: './on-changes.component.html',
+  styleUrls: ['./on-changes.component.less']
+})
+export class OnChangesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
