60ea97a0a718908a53188c06dcbafc1ffcef069d
specter01wj <specter01wj@gmail.com>
2020-04-22 16:14:22 -0600

    04/17/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/on-changes/on-changes-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/on-changes/on-changes-routing.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+import { OnChangesComponent } from './on-changes.component';
+import { OnChangesChildComponent } from './on-changes-child/on-changes-child.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: OnChangesComponent,
+    children: [
+      { path: 'on-changes-child', component: OnChangesChildComponent }
+    ]
+  }
+];
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class OnChangesRoutingModule { }
