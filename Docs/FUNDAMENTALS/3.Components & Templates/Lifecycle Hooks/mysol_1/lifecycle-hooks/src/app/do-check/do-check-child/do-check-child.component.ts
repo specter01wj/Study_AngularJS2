5e01c5f152e37565724e333eff717eaa53515b1d
specter01wj <specter01wj@gmail.com>
2020-04-22 17:54:31 -0600

    04/17/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/do-check/do-check-child/do-check-child.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-do-check-child',
+  templateUrl: './do-check-child.component.html',
+  styleUrls: ['./do-check-child.component.less']
+})
+export class DoCheckChildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/do-check/do-check.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/do-check/do-check.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DoCheckComponent } from './do-check.component';
+import { DoCheckChildComponent } from './do-check-child/do-check-child.component';
 
 
 
 @NgModule({
-  declarations: [DoCheckComponent],
+  declarations: [DoCheckComponent, DoCheckChildComponent],
   imports: [
     CommonModule
   ]
