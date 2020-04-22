66dcf07e37af11f85324e65e0dceee3b5f0a99dd
specter01wj <specter01wj@gmail.com>
2020-04-22 16:05:52 -0600

    04/17/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/on-changes/on-changes-child/on-changes-child.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/on-changes/on-changes-child/on-changes-child.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges,
+  SimpleChanges, ViewChild } from '@angular/core';
+
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-on-changes-child',
   templateUrl: './on-changes-child.component.html',
   styleUrls: ['./on-changes-child.component.less']
 })
-export class OnChangesChildComponent implements OnInit {
+export class OnChangesChildComponent implements OnInit, OnChanges {
+	@Input() hero: Hero;
+  @Input() power: string;
+
+  changeLog: string[] = [];
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  ngOnChanges(changes: SimpleChanges) {
+    for (let propName in changes) {
+      let chng = changes[propName];
+      let cur  = JSON.stringify(chng.currentValue);
+      let prev = JSON.stringify(chng.previousValue);
+      this.changeLog.push(`${propName}: currentValue = ${cur}, previousValue = ${prev}`);
+    }
+  }
+
+  reset() { this.changeLog = []; }
 
 }
