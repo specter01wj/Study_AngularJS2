77400ad3ba30979eb581451fae32270542a1b429
specter01wj <specter01wj@gmail.com>
2020-04-22 15:57:53 -0600

    04/17/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/on-changes/on-changes.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/on-changes/on-changes.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./on-changes.component.less']
 })
 export class OnChangesComponent implements OnInit {
+	hero: Hero;
+  power: string;
+  title = 'OnChanges';
+  @ViewChild(OnChangesComponent) childView: OnChangesComponent;
 
-  constructor() { }
+  constructor() { 
+  	this.reset();
+  }
 
   ngOnInit() {
+  }
+
+  reset() {
+    // new Hero object every time; triggers onChanges
+    this.hero = new Hero('Windstorm');
+    // setting power only triggers onChanges if this value is different
+    this.power = 'sing';
+    if (this.childView) { this.childView.reset(); }
   }
 
 }
