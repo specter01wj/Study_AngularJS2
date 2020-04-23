900bbc687879ff7becf657aa2b09f49a4089795d
specter01wj <specter01wj@gmail.com>
2020-04-23 16:48:16 -0600

    04/17/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/counter/counter.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/counter/counter.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { CounterRoutingModule } from './counter-routing.module';
+
 import { CounterComponent } from './counter.component';
 import { CounterChildComponent } from './counter-child/counter-child.component';
 
-
-
 @NgModule({
-  declarations: [CounterComponent, CounterChildComponent],
+  declarations: [
+  	CounterComponent, 
+  	CounterChildComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    CounterRoutingModule
+  ],
+  exports: []
 })
 export class CounterModule { }
