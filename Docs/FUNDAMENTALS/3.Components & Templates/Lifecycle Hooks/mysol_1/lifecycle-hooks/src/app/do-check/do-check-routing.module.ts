9ab2453d86502744b27157da920aec533b2e3524
specter01wj <specter01wj@gmail.com>
2020-04-22 21:26:47 -0600

    04/17/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/do-check/do-check-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/do-check/do-check-routing.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+import { DoCheckComponent } from './do-check.component';
+import { DoCheckChildComponent } from './do-check-child/do-check-child.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: DoCheckComponent,
+    children: [
+      { path: 'do-check-child', component: DoCheckChildComponent }
+    ]
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class DoCheckRoutingModule { }
