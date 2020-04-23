2560696b3a4536830f58e1da0c9e16b405685028
specter01wj <specter01wj@gmail.com>
2020-04-23 12:40:09 -0600

    04/17/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/after-content/after-content-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/after-content/after-content-routing.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+import { AfterContentComponent } from './after-content.component';
+import { AfterContentChildComponent } from './after-content-child/after-content-child.component';
+import { AfterContentGrandchildComponent } from './after-content-grandchild/after-content-grandchild.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: AfterContentComponent,
+    children: [
+      { path: 'after-content-child', component: AfterContentChildComponent },
+      { path: 'after-content-grandchild', component: AfterContentGrandchildComponent }
+    ]
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AfterContentRoutingModule { }
--- Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/after-content/after-content.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/after-content/after-content.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { AfterContentRoutingModule } from './after-content-routing.module';
+
 import { AfterContentComponent } from './after-content.component';
 import { AfterContentChildComponent } from './after-content-child/after-content-child.component';
 import { AfterContentGrandchildComponent } from './after-content-grandchild/after-content-grandchild.component';
 
-
-
 @NgModule({
-  declarations: [AfterContentComponent, AfterContentChildComponent, AfterContentGrandchildComponent],
+  declarations: [
+  	AfterContentComponent, 
+  	AfterContentChildComponent, 
+  	AfterContentGrandchildComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    AfterContentRoutingModule
+  ],
+  exports: []
 })
 export class AfterContentModule { }
