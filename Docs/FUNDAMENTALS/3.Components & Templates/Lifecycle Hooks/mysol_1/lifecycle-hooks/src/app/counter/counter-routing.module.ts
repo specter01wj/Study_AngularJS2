4bdec0c25dd057097ee235b69757eb81dec31140
specter01wj <specter01wj@gmail.com>
2020-04-23 16:49:47 -0600

    04/17/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/counter/counter-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/counter/counter-routing.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+import { CounterComponent } from './counter.component';
+import { CounterChildComponent } from './counter-child/counter-child.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: CounterComponent,
+    children: [
+      { path: 'counter-child', component: CounterChildComponent }
+    ]
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class CounterRoutingModule { }
