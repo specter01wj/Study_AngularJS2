9140ba6bdfe3137b3a424b516949cb35b795d611
specter01wj <specter01wj@gmail.com>
2020-04-23 17:07:56 -0600

    04/17/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/counter/counter-child/counter-child.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/counter/counter-child/counter-child.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input,
+  OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-counter-child',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./counter-child.component.less']
 })
 export class CounterChildComponent implements OnInit {
+	@Input() counter: number;
+  changeLog: string[] = [];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+
+    // Empty the changeLog whenever counter goes to zero
+    // hint: this is a way to respond programmatically to external value changes.
+    if (this.counter === 0) {
+      this.changeLog = [];
+    }
+
+    // A change to `counter` is the only change we care about
+    let chng = changes['counter'];
+    let cur = chng.currentValue;
+    let prev = JSON.stringify(chng.previousValue); // first time is {}; after is integer
+    this.changeLog.push(`counter: currentValue = ${cur}, previousValue = ${prev}`);
   }
 
 }
--- Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/counter/counter.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Lifecycle Hooks/mysol_1/lifecycle-hooks/src/app/counter/counter.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+
+import { LoggerService }  from '../services/logger.service';
 
 @Component({
   selector: 'app-counter',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./counter.component.less']
 })
 export class CounterComponent implements OnInit {
+	value: number;
+  spyLog: string[] = [];
 
-  constructor() { }
+  private logger: LoggerService;
+
+  constructor(logger: LoggerService) { 
+  	this.logger = logger;
+    this.spyLog = logger.logs;
+    this.reset();
+  }
 
   ngOnInit() {
+  }
+
+  updateCounter() {
+    this.value += 1;
+    this.logger.tick();
+  }
+
+  reset() {
+    this.logger.log('-- reset --');
+    this.value = 0;
+    this.logger.tick();
   }
 
 }
