e478a87252e688767d7c9f0c8ca978569684eedb
specter01wj <specter01wj@gmail.com>
2020-04-23 18:02:34 -0600

    04/17/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/app.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParentToChildComponent } from './parent-to-child/parent-to-child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentToChildComponent
   ],
   imports: [
     BrowserModule,
