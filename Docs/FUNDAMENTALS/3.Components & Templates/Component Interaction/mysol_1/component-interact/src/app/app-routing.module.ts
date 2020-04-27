caafa40a2eea6666611deb7c6cf14a717b998b88
specter01wj <specter01wj@gmail.com>
2020-04-26 23:27:42 -0600

    04/25/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/app-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+
 	{ path: '', redirectTo: '/parent-to-child', pathMatch: 'full' },
   /*{
     path: 'binding-syntax',
