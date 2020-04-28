ca192d0174d8f91adcfabcd7ccceb63317c64bdb
specter01wj <specter01wj@gmail.com>
2020-04-27 23:07:05 -0600

    04/25/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-setter/parent-to-child-setter-child/parent-to-child-setter-child.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-to-child-setter-child',
+  templateUrl: './parent-to-child-setter-child.component.html',
+  styleUrls: ['./parent-to-child-setter-child.component.less']
+})
+export class ParentToChildSetterChildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-setter/parent-to-child-setter.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-setter/parent-to-child-setter.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ParentToChildSetterComponent } from './parent-to-child-setter.component';
+import { ParentToChildSetterChildComponent } from './parent-to-child-setter-child.component';
 
 
 
 @NgModule({
-  declarations: [ParentToChildSetterComponent],
+  declarations: [ParentToChildSetterComponent, ParentToChildSetterChildComponent],
   imports: [
     CommonModule
   ]
