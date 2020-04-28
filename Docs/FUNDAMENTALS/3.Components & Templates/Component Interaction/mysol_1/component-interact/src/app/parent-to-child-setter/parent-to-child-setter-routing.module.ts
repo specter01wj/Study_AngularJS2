a158c31fe99ce4f4aa86069f65875b8de0539493
specter01wj <specter01wj@gmail.com>
2020-04-27 23:09:18 -0600

    04/25/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-setter/parent-to-child-setter-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-setter/parent-to-child-setter-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
-
+import { ParentToChildSetterComponent } from './parent-to-child-setter.component';
+import { ParentToChildSetterChildComponent } from './parent-to-child-setter-child/parent-to-child-setter-child.component';
 
 @NgModule({
   declarations: [],
