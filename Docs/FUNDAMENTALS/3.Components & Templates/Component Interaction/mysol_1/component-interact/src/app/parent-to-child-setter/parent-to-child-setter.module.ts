b675121125c82ecdb032dfbd481155c9adb59daa
specter01wj <specter01wj@gmail.com>
2020-04-27 23:08:15 -0600

    04/25/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-setter/parent-to-child-setter.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-setter/parent-to-child-setter.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { ParentToChildSetterRoutingModule } from './parent-to-child-setter-routing.module';
 
 import { ParentToChildSetterComponent } from './parent-to-child-setter.component';
-import { ParentToChildSetterChildComponent } from './parent-to-child-setter-child.component';
+import { ParentToChildSetterChildComponent } from './parent-to-child-setter-child/parent-to-child-setter-child.component';
 
 
 
