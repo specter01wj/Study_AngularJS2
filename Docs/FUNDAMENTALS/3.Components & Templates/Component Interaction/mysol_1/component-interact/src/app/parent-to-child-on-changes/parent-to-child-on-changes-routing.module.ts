5ea9c30e7f7774bef9268a5734226663072c9d86
specter01wj <specter01wj@gmail.com>
2020-04-29 17:54:33 -0600

    04/28/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-on-changes/parent-to-child-on-changes-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-on-changes/parent-to-child-on-changes-routing.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+import { ParentToChildOnChangesComponent } from './parent-to-child-on-changes.component';
+import { ParentToChildOnChangesChildComponent } from './parent-to-child-on-changes-child/parent-to-child-on-changes-child.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: ParentToChildOnChangesComponent,
+    children: [
+      { path: 'parent-to-child-on-changes-child', component: ParentToChildOnChangesComponent }
+    ]
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class ParentToChildOnChangesRoutingModule { }
