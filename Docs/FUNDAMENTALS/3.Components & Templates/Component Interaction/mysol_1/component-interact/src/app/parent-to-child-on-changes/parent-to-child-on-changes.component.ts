ba9de7df347665edad0439868fb44c4407797a4d
specter01wj <specter01wj@gmail.com>
2020-04-29 17:47:13 -0600

    04/28/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-on-changes/parent-to-child-on-changes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-to-child-on-changes',
+  templateUrl: './parent-to-child-on-changes.component.html',
+  styleUrls: ['./parent-to-child-on-changes.component.less']
+})
+export class ParentToChildOnChangesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-on-changes/parent-to-child-on-changes.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-on-changes/parent-to-child-on-changes.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ParentToChildOnChangesComponent } from './parent-to-child-on-changes.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ParentToChildOnChangesComponent],
   imports: [
     CommonModule
   ]
