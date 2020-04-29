f8448af03471eb840f9c1cbb519c5fd756cc8928
specter01wj <specter01wj@gmail.com>
2020-04-29 17:48:22 -0600

    04/28/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-on-changes/parent-to-child-on-changes-child/parent-to-child-on-changes-child.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-to-child-on-changes-child',
+  templateUrl: './parent-to-child-on-changes-child.component.html',
+  styleUrls: ['./parent-to-child-on-changes-child.component.less']
+})
+export class ParentToChildOnChangesChildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-on-changes/parent-to-child-on-changes.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-on-changes/parent-to-child-on-changes.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ParentToChildOnChangesComponent } from './parent-to-child-on-changes.component';
+import { ParentToChildOnChangesChildComponent } from './parent-to-child-on-changes-child.component';
 
 
 
 @NgModule({
-  declarations: [ParentToChildOnChangesComponent],
+  declarations: [ParentToChildOnChangesComponent, ParentToChildOnChangesChildComponent],
   imports: [
     CommonModule
   ]
