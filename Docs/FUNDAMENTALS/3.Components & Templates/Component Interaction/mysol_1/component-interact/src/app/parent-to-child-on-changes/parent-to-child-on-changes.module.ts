b5e4bcf5da8385996ce88ec3ea87bc47ca5aecdc
specter01wj <specter01wj@gmail.com>
2020-04-29 17:50:05 -0600

    04/28/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-on-changes/parent-to-child-on-changes.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-on-changes/parent-to-child-on-changes.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { ParentToChildOnChangesRoutingModule } from './parent-to-child-on-changes-routing.module';
+
+
 import { ParentToChildOnChangesComponent } from './parent-to-child-on-changes.component';
-import { ParentToChildOnChangesChildComponent } from './parent-to-child-on-changes-child.component';
+import { ParentToChildOnChangesChildComponent } from './parent-to-child-on-changes-child/parent-to-child-on-changes-child.component';
 
 
 
