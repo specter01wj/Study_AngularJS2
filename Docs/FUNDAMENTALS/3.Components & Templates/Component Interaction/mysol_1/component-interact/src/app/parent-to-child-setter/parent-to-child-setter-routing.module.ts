4d854b2cc8f8be8b8022e6b811b8babc29f6c340
specter01wj <specter01wj@gmail.com>
2020-04-28 22:36:11 -0600

    04/28/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-setter/parent-to-child-setter-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-setter/parent-to-child-setter-routing.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
     path: '',
     component: ParentToChildSetterComponent,
 
+
     
     children: [
       { path: 'parent-to-child-setter-child', component: ParentToChildSetterChildComponent }
