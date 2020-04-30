931942edac4b9eac0f31c0820151efc80db87367
specter01wj <specter01wj@gmail.com>
2020-04-30 17:21:44 -0600

    04/28/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-local-var/parent-to-child-local-var-child/parent-to-child-local-var-child.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-to-child-local-var-child',
+  templateUrl: './parent-to-child-local-var-child.component.html',
+  styleUrls: ['./parent-to-child-local-var-child.component.less']
+})
+export class ParentToChildLocalVarChildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-local-var/parent-to-child-local-var.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-local-var/parent-to-child-local-var.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ParentToChildLocalVarComponent } from './parent-to-child-local-var.component';
+import { ParentToChildLocalVarChildComponent } from './parent-to-child-local-var-child/parent-to-child-local-var-child.component';
 
 
 
 @NgModule({
-  declarations: [ParentToChildLocalVarComponent],
+  declarations: [ParentToChildLocalVarComponent, ParentToChildLocalVarChildComponent],
   imports: [
     CommonModule
   ]
