22d46f408041bfda83e43aec9ce3a5c8b6ecf77d
specter01wj <specter01wj@gmail.com>
2020-04-29 22:07:42 -0600

    04/28/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/child-to-parent/child-to-parent.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/child-to-parent/child-to-parent.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChildToParentComponent } from './child-to-parent.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ChildToParentComponent],
   imports: [
     CommonModule
   ]
