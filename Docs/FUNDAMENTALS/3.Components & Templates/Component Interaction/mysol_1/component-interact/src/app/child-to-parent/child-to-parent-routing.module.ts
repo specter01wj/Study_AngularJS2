49d7f9c51fac903021aaba22d34d01719cbf9e63
specter01wj <specter01wj@gmail.com>
2020-04-29 22:12:08 -0600

    04/28/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/child-to-parent/child-to-parent-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/child-to-parent/child-to-parent-routing.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+import { ChildToParentComponent } from './child-to-parent.component';
+import { ChildToParentChildComponent } from './child-to-parent-child/child-to-parent-child.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: ChildToParentComponent,
+    children: [
+      { path: 'child-to-parent-child', component: ChildToParentChildComponent }
+    ]
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class ChildToParentRoutingModule { }
--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/child-to-parent/child-to-parent.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/child-to-parent/child-to-parent.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { ChildToParentRoutingModule } from './child-to-parent-routing.module';
+
 import { ChildToParentComponent } from './child-to-parent.component';
 import { ChildToParentChildComponent } from './child-to-parent-child/child-to-parent-child.component';
 
-
-
 @NgModule({
-  declarations: [ChildToParentComponent, ChildToParentChildComponent],
+  declarations: [
+  	ChildToParentComponent, 
+  	ChildToParentChildComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    ChildToParentRoutingModule
+  ],
+  exports: []
 })
 export class ChildToParentModule { }
