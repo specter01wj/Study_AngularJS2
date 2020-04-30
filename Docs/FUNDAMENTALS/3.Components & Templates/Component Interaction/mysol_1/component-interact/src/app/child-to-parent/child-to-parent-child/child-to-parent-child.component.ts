fc82dac527155db11da1f5dbfd9bd99d02654749
specter01wj <specter01wj@gmail.com>
2020-04-29 22:08:04 -0600

    04/28/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/child-to-parent/child-to-parent-child/child-to-parent-child.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child-to-parent-child',
+  templateUrl: './child-to-parent-child.component.html',
+  styleUrls: ['./child-to-parent-child.component.less']
+})
+export class ChildToParentChildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/child-to-parent/child-to-parent.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/child-to-parent/child-to-parent.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChildToParentComponent } from './child-to-parent.component';
+import { ChildToParentChildComponent } from './child-to-parent-child/child-to-parent-child.component';
 
 
 
 @NgModule({
-  declarations: [ChildToParentComponent],
+  declarations: [ChildToParentComponent, ChildToParentChildComponent],
   imports: [
     CommonModule
   ]
