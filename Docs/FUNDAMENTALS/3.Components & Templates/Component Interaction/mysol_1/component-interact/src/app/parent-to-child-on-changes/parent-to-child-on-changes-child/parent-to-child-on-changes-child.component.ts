2ec83e55a5ba69c3c70f7111327969eb0ac34ab5
specter01wj <specter01wj@gmail.com>
2020-04-29 18:00:25 -0600

    04/28/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-on-changes/parent-to-child-on-changes-child/parent-to-child-on-changes-child.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-on-changes/parent-to-child-on-changes-child/parent-to-child-on-changes-child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChange } from '@angular/core';
 
 @Component({
   selector: 'app-parent-to-child-on-changes-child',
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parent-to-child-on-changes-child.component.less']
 })
 export class ParentToChildOnChangesChildComponent implements OnInit {
+	@Input() major: number;
+  @Input() minor: number;
+  changeLog: string[] = [];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: {[propKey: string]: SimpleChange}) {
+    let log: string[] = [];
+    for (let propName in changes) {
+      let changedProp = changes[propName];
+      let to = JSON.stringify(changedProp.currentValue);
+      if (changedProp.isFirstChange()) {
+        log.push(`Initial value of ${propName} set to ${to}`);
+      } else {
+        let from = JSON.stringify(changedProp.previousValue);
+        log.push(`${propName} changed from ${from} to ${to}`);
+      }
+    }
+    this.changeLog.push(log.join(', '));
   }
 
 }
