d197299ba265bb21b6cbdb5d32a5e12f57c667e1
specter01wj <specter01wj@gmail.com>
2020-04-30 17:23:14 -0600

    04/28/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-local-var/parent-to-child-local-var.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-child-local-var/parent-to-child-local-var.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { ParentToChildLocalVarRoutingModule } from './parent-to-child-local-var-routing.module';
+
 import { ParentToChildLocalVarComponent } from './parent-to-child-local-var.component';
 import { ParentToChildLocalVarChildComponent } from './parent-to-child-local-var-child/parent-to-child-local-var-child.component';
 
-
-
 @NgModule({
-  declarations: [ParentToChildLocalVarComponent, ParentToChildLocalVarChildComponent],
+  declarations: [
+  	ParentToChildLocalVarComponent, 
+  	ParentToChildLocalVarChildComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    ParentToChildLocalVarRoutingModule
+  ],
+  exports: []
 })
 export class ParentToChildLocalVarModule { }
