2d63bc6b85803e820e44f60462bb2c18ac2cc014
specter01wj <specter01wj@gmail.com>
2020-04-30 22:55:44 -0600

    04/28/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-view-child/parent-to-view-child.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-view-child/parent-to-view-child.component.ts
@@ -8,12 +8,24 @@ import { ParentToViewChildTimerComponent }  from './parent-to-view-child-timer/p
   styleUrls: ['./parent-to-view-child.component.less']
 })
 export class ParentToViewChildComponent implements OnInit, AfterViewInit {
-	@ViewChild(ParentToViewChildTimerComponent, {static: false})
+	@ViewChild(ParentToViewChildTimerComponent, {static: false});
   private timerComponent: ParentToViewChildTimerComponent;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  seconds() { return 0; }
+
+  ngAfterViewInit() {
+    // Redefine `seconds()` to get from the `CountdownTimerComponent.seconds` ...
+    // but wait a tick first to avoid one-time devMode
+    // unidirectional-data-flow-violation error
+    setTimeout(() => this.seconds = () => this.timerComponent.seconds, 0);
+  }
+
+  start() { this.timerComponent.start(); }
+  stop() { this.timerComponent.stop(); }
 
 }
