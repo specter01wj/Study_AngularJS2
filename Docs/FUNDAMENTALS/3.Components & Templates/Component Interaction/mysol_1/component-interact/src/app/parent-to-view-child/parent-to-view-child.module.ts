24a06d5f3982b91c05605eb81f48b10a532220f1
specter01wj <specter01wj@gmail.com>
2020-04-30 22:46:38 -0600

    04/28/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-view-child/parent-to-view-child.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-view-child/parent-to-view-child.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { ParentToViewChildRoutingModule } from './parent-to-view-child-routing.module';
+
 import { ParentToViewChildComponent } from './parent-to-view-child.component';
 import { ParentToViewChildTimerComponent } from './parent-to-view-child-timer/parent-to-view-child-timer.component';
 
 
 
 @NgModule({
-  declarations: [ParentToViewChildComponent, ParentToViewChildTimerComponent],
+  declarations: [
+  	ParentToViewChildComponent, 
+  	ParentToViewChildTimerComponent
+  ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: []
 })
 export class ParentToViewChildModule { }
