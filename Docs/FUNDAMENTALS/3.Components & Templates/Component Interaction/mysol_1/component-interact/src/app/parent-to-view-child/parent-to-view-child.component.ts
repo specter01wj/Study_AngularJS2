20c697fa45d7ce79bd9ec49e384585249b6a9902
specter01wj <specter01wj@gmail.com>
2020-04-30 22:52:47 -0600

    04/28/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-view-child/parent-to-view-child.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-view-child/parent-to-view-child.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+
+import { ParentToViewChildTimerComponent }  from './parent-to-view-child-timer/parent-to-view-child-timer.component';
 
 @Component({
   selector: 'app-parent-to-view-child',
   templateUrl: './parent-to-view-child.component.html',
   styleUrls: ['./parent-to-view-child.component.less']
 })
-export class ParentToViewChildComponent implements OnInit {
+export class ParentToViewChildComponent implements OnInit, AfterViewInit {
+	@ViewChild(ParentToViewChildTimerComponent, {static: false})
+  private timerComponent: ParentToViewChildTimerComponent;
 
   constructor() { }
 
