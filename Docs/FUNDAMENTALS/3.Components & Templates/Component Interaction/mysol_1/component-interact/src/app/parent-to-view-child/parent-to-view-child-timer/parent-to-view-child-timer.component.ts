e2c25065de542385c0bbe3b7c47f78794a157a84
specter01wj <specter01wj@gmail.com>
2020-04-30 22:54:42 -0600

    04/28/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-view-child/parent-to-view-child-timer/parent-to-view-child-timer.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-view-child/parent-to-view-child-timer/parent-to-view-child-timer.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-parent-to-view-child-timer',
   templateUrl: './parent-to-view-child-timer.component.html',
   styleUrls: ['./parent-to-view-child-timer.component.less']
 })
-export class ParentToViewChildTimerComponent implements OnInit {
+export class ParentToViewChildTimerComponent implements OnInit, OnDestroy {
+	intervalId = 0;
+  message = '';
+  seconds = 11;
 
   constructor() { }
 
   ngOnInit() {
+  	this.start();
+  }
+
+  clearTimer() { clearInterval(this.intervalId); }
+  
+  ngOnDestroy() { this.clearTimer(); }
+
+  start() { this.countDown(); }
+  stop()  {
+    this.clearTimer();
+    this.message = `Holding at T-${this.seconds} seconds`;
+  }
+
+  private countDown() {
+    this.clearTimer();
+    this.intervalId = window.setInterval(() => {
+      this.seconds -= 1;
+      if (this.seconds === 0) {
+        this.message = 'Blast off!';
+      } else {
+        if (this.seconds < 0) { this.seconds = 10; } // reset
+        this.message = `T-${this.seconds} seconds and counting`;
+      }
+    }, 1000);
   }
 
 }
