b26f5279de23a9b2b9acc9008e11dbec69ce81c3
specter01wj <specter01wj@gmail.com>
2020-04-30 22:43:54 -0600

    04/28/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-view-child/parent-to-view-child-timer/parent-to-view-child-timer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent-to-view-child-timer',
+  templateUrl: './parent-to-view-child-timer.component.html',
+  styleUrls: ['./parent-to-view-child-timer.component.less']
+})
+export class ParentToViewChildTimerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-view-child/parent-to-view-child.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/parent-to-view-child/parent-to-view-child.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ParentToViewChildComponent } from './parent-to-view-child.component';
+import { ParentToViewChildTimerComponent } from './parent-to-view-child-timer/parent-to-view-child-timer.component';
 
 
 
 @NgModule({
-  declarations: [ParentToViewChildComponent],
+  declarations: [ParentToViewChildComponent, ParentToViewChildTimerComponent],
   imports: [
     CommonModule
   ]
