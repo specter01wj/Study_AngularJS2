1f1450f1e7285be04c233dedfe7f8993cb0f3957
specter01wj <specter01wj@gmail.com>
2020-05-01 18:09:11 -0600

    05/01/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/bidirectional-service/bidirectional-service-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/bidirectional-service/bidirectional-service-routing.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+import { BidirectionalServiceComponent } from './bidirectional-service.component';
+import { AstronautComponent } from './astronaut/astronaut.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: BidirectionalServiceComponent,
+    children: [
+      { path: 'astronaut', component: AstronautComponent }
+    ]
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class BidirectionalServiceRoutingModule { }
