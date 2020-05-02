0673955bb160a0c9cfc7fad4a2295ca1af5a3a1a
specter01wj <specter01wj@gmail.com>
2020-05-02 17:20:32 -0600

    05/02/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Component Styles/mysol_1/component-style/src/app/app.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Styles/mysol_1/component-style/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroAppMainComponent } from './hero-app-main/hero-app-main.component';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroAppMainComponent
+    HeroAppMainComponent,
+    HeroDetailsComponent
   ],
   imports: [
     BrowserModule,
