074c0d8eb814e78f36430c1f1a845d82803c9388
specter01wj <specter01wj@gmail.com>
2020-05-01 18:13:26 -0600

    05/01/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/bidirectional-service/bidirectional-service.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/bidirectional-service/bidirectional-service.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MissionService }     from '../services/mission.service';
 
 @Component({
   selector: 'app-bidirectional-service',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bidirectional-service.component.less']
 })
 export class BidirectionalServiceComponent implements OnInit {
+	astronauts = ['Lovell', 'Swigert', 'Haise'];
+  history: string[] = [];
+  missions = ['Fly to the moon!',
+              'Fly to mars!',
+              'Fly to Vegas!'];
+  nextMission = 0;
 
-  constructor() { }
+  constructor(private missionService: MissionService) { 
+  	missionService.missionConfirmed$.subscribe(
+      astronaut => {
+        this.history.push(`${astronaut} confirmed the mission`);
+      }
+    );
+  }
 
   ngOnInit() {
+  }
+
+  announce() {
+    let mission = this.missions[this.nextMission++];
+    this.missionService.announceMission(mission);
+    this.history.push(`Mission "${mission}" announced`);
+    if (this.nextMission >= this.missions.length) { this.nextMission = 0; }
   }
 
 }
