c7744479c13b697367d713de57f4e3968c916da5
specter01wj <specter01wj@gmail.com>
2020-05-01 18:15:08 -0600

    05/01/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/bidirectional-service/astronaut/astronaut.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Interaction/mysol_1/component-interact/src/app/bidirectional-service/astronaut/astronaut.component.ts
@@ -1,13 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+
+import { MissionService } from '../../services/mission.service';
+import { Subscription }   from 'rxjs';
 
 @Component({
   selector: 'app-astronaut',
   templateUrl: './astronaut.component.html',
   styleUrls: ['./astronaut.component.less']
 })
-export class AstronautComponent implements OnInit {
+export class AstronautComponent implements OnInit, OnDestroy {
+	@Input() astronaut: string;
+  mission = '<no mission announced>';
+  confirmed = false;
+  announced = false;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private missionService: MissionService) { 
+  	this.subscription = missionService.missionAnnounced$.subscribe(
+      mission => {
+        this.mission = mission;
+        this.announced = true;
+        this.confirmed = false;
+    });
+  }
 
   ngOnInit() {
   }
