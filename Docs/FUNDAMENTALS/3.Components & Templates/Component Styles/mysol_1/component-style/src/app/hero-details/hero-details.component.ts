41e80f7e438d1a29daa480313445a6e6d358379c
specter01wj <specter01wj@gmail.com>
2020-05-02 17:51:26 -0600

    05/02/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Component Styles/mysol_1/component-style/src/app/hero-details/hero-details.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Styles/mysol_1/component-style/src/app/hero-details/hero-details.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Hero } from '../services/hero';
 
 @Component({
   selector: 'app-hero-details',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-details.component.less']
 })
 export class HeroDetailsComponent implements OnInit {
+	@Input() hero: Hero;
 
   constructor() { }
 
