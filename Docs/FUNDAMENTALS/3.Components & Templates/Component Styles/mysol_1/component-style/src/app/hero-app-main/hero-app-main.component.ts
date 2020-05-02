7a141ffa7b8845ebb35d68fb643760de7ea33d0f
specter01wj <specter01wj@gmail.com>
2020-05-02 17:19:57 -0600

    05/02/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Component Styles/mysol_1/component-style/src/app/hero-app-main/hero-app-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-app-main',
+  templateUrl: './hero-app-main.component.html',
+  styleUrls: ['./hero-app-main.component.less']
+})
+export class HeroAppMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
