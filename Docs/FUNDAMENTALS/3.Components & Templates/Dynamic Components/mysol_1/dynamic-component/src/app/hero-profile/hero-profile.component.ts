921df18b1a734b1929b4267864d3eb7a0d49f65f
specter01wj <specter01wj@gmail.com>
2020-05-04 22:47:22 -0600

    05/02/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Dynamic Components/mysol_1/dynamic-component/src/app/hero-profile/hero-profile.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Dynamic Components/mysol_1/dynamic-component/src/app/hero-profile/hero-profile.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { AdComponent } from '../services/ad';
 
 @Component({
   selector: 'app-hero-profile',
   templateUrl: './hero-profile.component.html',
   styleUrls: ['./hero-profile.component.less']
 })
-export class HeroProfileComponent implements OnInit {
+export class HeroProfileComponent implements OnInit, AdComponent {
+	@Input() data: any;
 
   constructor() { }
 
