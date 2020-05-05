c710c036b284c0cbf6d13878450ce65f033dd267
specter01wj <specter01wj@gmail.com>
2020-05-04 22:37:42 -0600

    05/02/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Dynamic Components/mysol_1/dynamic-component/src/app/app.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Dynamic Components/mysol_1/dynamic-component/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroJobAdComponent } from './hero-job-ad/hero-job-ad.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroJobAdComponent
   ],
   imports: [
     BrowserModule,
---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Dynamic Components/mysol_1/dynamic-component/src/app/hero-job-ad/hero-job-ad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-job-ad',
+  templateUrl: './hero-job-ad.component.html',
+  styleUrls: ['./hero-job-ad.component.less']
+})
+export class HeroJobAdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
