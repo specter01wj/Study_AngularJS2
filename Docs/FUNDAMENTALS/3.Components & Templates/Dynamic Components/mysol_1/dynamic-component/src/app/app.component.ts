5cf3ea77d0cded04db911b18d64f228c9f57be8b
specter01wj <specter01wj@gmail.com>
2020-05-04 22:34:13 -0600

    05/02/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Dynamic Components/mysol_1/dynamic-component/src/app/app.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Dynamic Components/mysol_1/dynamic-component/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AdService } from './ad.service';
+import { AdItem } from './ad-item';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
