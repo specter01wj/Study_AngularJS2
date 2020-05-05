74946762232de7044408f15d3efc8b2d9c429917
specter01wj <specter01wj@gmail.com>
2020-05-04 22:40:24 -0600

    05/02/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Dynamic Components/mysol_1/dynamic-component/src/app/services/ad.service.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Dynamic Components/mysol_1/dynamic-component/src/app/services/ad.service.ts
@@ -1,7 +1,7 @@
 import { Injectable }           from '@angular/core';
 
 import { HeroJobAdComponent }   from '../hero-job-ad/hero-job-ad.component';
-import { HeroProfileComponent } from './hero-profile.component';
+import { HeroProfileComponent } from '../hero-profile/hero-profile.component';
 import { AdItem }               from './ad-item';
 
 @Injectable()
