1eef7a5d900190b39f52f4b9a8057c7e30f93d53
specter01wj <specter01wj@gmail.com>
2020-05-04 22:41:13 -0600

    05/02/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Dynamic Components/mysol_1/dynamic-component/src/app/services/ad.service.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Dynamic Components/mysol_1/dynamic-component/src/app/services/ad.service.ts
@@ -4,7 +4,9 @@ import { HeroJobAdComponent }   from '../hero-job-ad/hero-job-ad.component';
 import { HeroProfileComponent } from '../hero-profile/hero-profile.component';
 import { AdItem }               from './ad-item';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class AdService {
   getAds() {
     return [
