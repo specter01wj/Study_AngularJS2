71cdcf5ff1ee3aa366adb9e9659e1830636e00be
specter01wj <specter01wj@gmail.com>
2020-05-06 22:51:00 -0600

    05/06/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Attribute Directives/mysol_1/attribute-directive/src/app/app.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Attribute Directives/mysol_1/attribute-directive/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
