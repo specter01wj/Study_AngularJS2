930ae17d8c2ca7883a42e375288db231bb968f8d
specter01wj <specter01wj@gmail.com>
2020-05-06 22:53:03 -0600

    05/06/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Attribute Directives/mysol_1/attribute-directive/src/app/directives/highlight.directive.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Attribute Directives/mysol_1/attribute-directive/src/app/directives/highlight.directive.ts
@@ -7,7 +7,19 @@ export class HighlightDirective {
 	@Input() defaultColor: string;
 
   @Input('appHighlight') highlightColor: string;
-	
+
   constructor(private el: ElementRef) { }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight(this.highlightColor || this.defaultColor || 'red');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
 
 }
