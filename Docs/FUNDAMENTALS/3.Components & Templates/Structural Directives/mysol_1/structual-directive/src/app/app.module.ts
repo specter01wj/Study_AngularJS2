f8b862a8bdcedeb4818a672c27c5fb8afa89f113
specter01wj <specter01wj@gmail.com>
2020-05-07 22:44:56 -0600

    05/06/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Structural Directives/mysol_1/structual-directive/src/app/app.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Structural Directives/mysol_1/structual-directive/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { UnlessDirective } from './directives/unless.directive';
 import { HeroSwitchComponent } from './hero-switch/hero-switch.component';
@@ -14,6 +16,7 @@ import { HeroSwitchComponent } from './hero-switch/hero-switch.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
