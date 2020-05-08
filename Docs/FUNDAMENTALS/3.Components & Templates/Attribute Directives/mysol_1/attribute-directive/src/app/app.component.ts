1e91bcad889e23d5b055eb52230535d0e62895f6
specter01wj <specter01wj@gmail.com>
2020-05-07 18:12:40 -0600

    05/06/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Attribute Directives/mysol_1/attribute-directive/src/app/app.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Attribute Directives/mysol_1/attribute-directive/src/app/app.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'AttributeDirective';
   color: string;
+
+  clickColor(color): void {
+  	this.color = color;
+  }
+
 }
