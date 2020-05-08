25b215c85fb2d0acd7d2476bd8cd9a89bd718eb6
specter01wj <specter01wj@gmail.com>
2020-05-07 22:45:50 -0600

    05/06/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Structural Directives/mysol_1/structual-directive/src/app/directives/unless.directive.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Structural Directives/mysol_1/structual-directive/src/app/directives/unless.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appUnless]'
 })
 export class UnlessDirective {
+  private hasView = false;
 
-  constructor() { }
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef) { }
+
+  @Input() set appUnless(condition: boolean) {
+    if (!condition && !this.hasView) {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+      this.hasView = true;
+    } else if (condition && this.hasView) {
+      this.viewContainer.clear();
+      this.hasView = false;
+    }
+  }
 
 }
