b9c21e70fd7983fdcc573bbdcccae9ca6465d559
specter01wj <specter01wj@gmail.com>
2020-05-07 22:48:38 -0600

    05/06/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Structural Directives/mysol_1/structual-directive/src/app/hero-switch/hero-switch.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Structural Directives/mysol_1/structual-directive/src/app/hero-switch/hero-switch.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Hero } from '../services/hero';
 
 @Component({
-  selector: 'app-hero-switch',
-  templateUrl: './hero-switch.component.html',
-  styleUrls: ['./hero-switch.component.less']
+  selector: 'app-happy-hero',
+  template: `Wow. You like {{hero.name}}. What a happy hero ... just like you.`
 })
-export class HeroSwitchComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HappyHeroComponent {
+  @Input() hero: Hero;
 }
+
+@Component({
+  selector: 'app-sad-hero',
+  template: `You like {{hero.name}}? Such a sad hero. Are you sad too?`
+})
+export class SadHeroComponent {
+  @Input() hero: Hero;
+}
+
+@Component({
+  selector: 'app-confused-hero',
+  template: `Are you as confused as {{hero.name}}?`
+})
+export class ConfusedHeroComponent {
+  @Input() hero: Hero;
+}
+
+@Component({
+  selector: 'app-unknown-hero',
+  template: `{{message}}`
+})
+export class UnknownHeroComponent {
+  @Input() hero: Hero;
+  get message() {
+    return this.hero && this.hero.name ?
+      `${this.hero.name} is strange and mysterious.` :
+      'Are you feeling indecisive?';
+  }
+}
+
+export const HeroSwitchComponent = 
+[ HappyHeroComponent, SadHeroComponent, ConfusedHeroComponent, UnknownHeroComponent ];
