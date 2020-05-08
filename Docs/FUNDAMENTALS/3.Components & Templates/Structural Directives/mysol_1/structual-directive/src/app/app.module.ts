398cc097f7f8967a06926ea346ef1e62944b8c34
specter01wj <specter01wj@gmail.com>
2020-05-07 22:37:17 -0600

    05/06/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Structural Directives/mysol_1/structual-directive/src/app/app.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Structural Directives/mysol_1/structual-directive/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UnlessDirective } from './directives/unless.directive';
+import { HeroSwitchComponent } from './hero-switch/hero-switch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UnlessDirective
+    UnlessDirective,
+    HeroSwitchComponent
   ],
   imports: [
     BrowserModule,
---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Structural Directives/mysol_1/structual-directive/src/app/hero-switch/hero-switch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-switch',
+  templateUrl: './hero-switch.component.html',
+  styleUrls: ['./hero-switch.component.less']
+})
+export class HeroSwitchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
