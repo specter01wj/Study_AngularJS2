93e2307935f15ea9df7f4c41615c2b95e10f963c
specter01wj <specter01wj@gmail.com>
2020-05-10 22:44:20 -0600

    05/10/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/power-booster/power-booster.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-power-booster',
+  templateUrl: './power-booster.component.html',
+  styleUrls: ['./power-booster.component.less']
+})
+export class PowerBoosterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/power-booster/power-booster.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PowerBoosterComponent } from './power-booster.component';
+
+
+
+@NgModule({
+  declarations: [PowerBoosterComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PowerBoosterModule { }
