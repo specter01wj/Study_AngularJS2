ccfbb8e247d8c3a1c38796b2bb8a8ce84f0fa4ce
specter01wj <specter01wj@gmail.com>
2020-05-10 22:04:12 -0600

    05/10/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/app-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', redirectTo: '/happy-birthday1', pathMatch: 'full' },
+  /*{
+    path: 'binding-syntax',
+    loadChildren: () => import('./binding-syntax/binding-syntax.module').then(m => m.BindingSyntaxModule)
+  },*/
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
