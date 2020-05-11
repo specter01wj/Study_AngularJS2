e323d531246d25962fa21be2cdb0da41c5a38387
specter01wj <specter01wj@gmail.com>
2020-05-10 22:46:09 -0600

    05/10/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/flying-heroes/flying-heroes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flying-heroes',
+  templateUrl: './flying-heroes.component.html',
+  styleUrls: ['./flying-heroes.component.less']
+})
+export class FlyingHeroesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/flying-heroes/flying-heroes.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlyingHeroesComponent } from './flying-heroes.component';
+
+
+
+@NgModule({
+  declarations: [FlyingHeroesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FlyingHeroesModule { }
