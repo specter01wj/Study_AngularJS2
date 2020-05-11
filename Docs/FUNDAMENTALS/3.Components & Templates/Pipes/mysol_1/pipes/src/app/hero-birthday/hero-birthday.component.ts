aeb8216eaae96f543d0d8ba8963ae197d59ce505
specter01wj <specter01wj@gmail.com>
2020-05-10 22:07:33 -0600

    05/10/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/hero-birthday/hero-birthday.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-birthday',
+  templateUrl: './hero-birthday.component.html',
+  styleUrls: ['./hero-birthday.component.less']
+})
+export class HeroBirthdayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/hero-birthday/hero-birthday.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroBirthdayComponent } from './hero-birthday.component';
+
+
+
+@NgModule({
+  declarations: [HeroBirthdayComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeroBirthdayModule { }
