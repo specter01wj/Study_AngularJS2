1d5e90a2a552aef61136e81f0caf34c26bcc65bf
specter01wj <specter01wj@gmail.com>
2020-05-10 22:10:38 -0600

    05/10/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/hero-birthday/hero-birthday.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/hero-birthday/hero-birthday.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { HeroBirthdayRoutingModule } from './hero-birthday-routing.module';
+
 import { HeroBirthdayComponent } from './hero-birthday.component';
 
-
-
 @NgModule({
-  declarations: [HeroBirthdayComponent],
+  declarations: [
+  	HeroBirthdayComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    HeroBirthdayRoutingModule
+  ],
+  exports: []
 })
 export class HeroBirthdayModule { }
