28ec6b352a0895f138ec8ab5ce8fc29ff4082d4f
specter01wj <specter01wj@gmail.com>
2020-05-10 22:54:18 -0600

    05/10/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/birthday-date-pipe/birthday-date-pipe-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/birthday-date-pipe/birthday-date-pipe-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+import { BirthdayDatePipeComponent } from './birthday-date-pipe.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: BirthdayDatePipeComponent,
+    children: [
+    ]
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class BirthdayDatePipeRoutingModule { }
--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/birthday-date-pipe/birthday-date-pipe.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/birthday-date-pipe/birthday-date-pipe.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { BirthdayDatePipeRoutingModule } from './birthday-date-pipe-routing.module';
+
 import { BirthdayDatePipeComponent } from './birthday-date-pipe.component';
 
-
-
 @NgModule({
-  declarations: [BirthdayDatePipeComponent],
+  declarations: [
+  	BirthdayDatePipeComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    BirthdayDatePipeRoutingModule
+  ],
+  exports: []
 })
 export class BirthdayDatePipeModule { }
