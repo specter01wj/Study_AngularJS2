5570bfea90fd07c341433ac282d68b545ef1359a
specter01wj <specter01wj@gmail.com>
2020-05-10 22:46:47 -0600

    05/10/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/flying-heroes-impure/flying-heroes-impure.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flying-heroes-impure',
+  templateUrl: './flying-heroes-impure.component.html',
+  styleUrls: ['./flying-heroes-impure.component.less']
+})
+export class FlyingHeroesImpureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
