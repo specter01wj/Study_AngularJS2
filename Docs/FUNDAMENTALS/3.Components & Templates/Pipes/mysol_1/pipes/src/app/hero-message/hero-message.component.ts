2649c2932efa4b500590a6c0da15f15665080123
specter01wj <specter01wj@gmail.com>
2020-05-10 22:47:32 -0600

    05/10/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/hero-message/hero-message.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-message',
+  templateUrl: './hero-message.component.html',
+  styleUrls: ['./hero-message.component.less']
+})
+export class HeroMessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/hero-message/hero-message.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroMessageComponent } from './hero-message.component';
+
+
+
+@NgModule({
+  declarations: [HeroMessageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeroMessageModule { }
