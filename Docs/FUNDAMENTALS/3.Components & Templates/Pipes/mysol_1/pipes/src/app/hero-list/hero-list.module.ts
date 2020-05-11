38ff1c08940740d1557204e482c2ee58121b591b
specter01wj <specter01wj@gmail.com>
2020-05-10 22:48:10 -0600

    05/10/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/hero-list/hero-list.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroListComponent } from './hero-list.component';
+
+
+
+@NgModule({
+  declarations: [HeroListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeroListModule { }
