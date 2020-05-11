a4ecde4b89b2d5ca2e151a167eab0ee8f1afc240
specter01wj <specter01wj@gmail.com>
2020-05-10 22:12:13 -0600

    05/10/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/hero-birthday/hero-birthday-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/hero-birthday/hero-birthday-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+import { HeroBirthdayComponent } from './hero-birthday.component';
 
+const routes: Routes = [
+  {
+    path: 'happy-birthday1',
+    component: HeroBirthdayComponent,
+    children: [
+    ]
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class HeroBirthdayRoutingModule { }
