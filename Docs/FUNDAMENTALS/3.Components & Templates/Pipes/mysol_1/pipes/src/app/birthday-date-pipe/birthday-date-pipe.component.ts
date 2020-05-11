8689b1f177d5fc1f20ae89fec865ddea85d3d615
specter01wj <specter01wj@gmail.com>
2020-05-10 22:30:02 -0600

    05/10/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/birthday-date-pipe/birthday-date-pipe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-birthday-date-pipe',
+  templateUrl: './birthday-date-pipe.component.html',
+  styleUrls: ['./birthday-date-pipe.component.less']
+})
+export class BirthdayDatePipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/birthday-date-pipe/birthday-date-pipe.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BirthdayDatePipeComponent } from './birthday-date-pipe.component';
+
+
+
+@NgModule({
+  declarations: [BirthdayDatePipeComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BirthdayDatePipeModule { }
