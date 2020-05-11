ccdf2e952b23a78d5e02f785e2a628b05f0ca9d9
specter01wj <specter01wj@gmail.com>
2020-05-10 22:44:58 -0600

    05/10/2020 v1

---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/power-boost-calculator/power-boost-calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-power-boost-calculator',
+  templateUrl: './power-boost-calculator.component.html',
+  styleUrls: ['./power-boost-calculator.component.less']
+})
+export class PowerBoostCalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/power-boost-calculator/power-boost-calculator.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PowerBoostCalculatorComponent } from './power-boost-calculator.component';
+
+
+
+@NgModule({
+  declarations: [PowerBoostCalculatorComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PowerBoostCalculatorModule { }
