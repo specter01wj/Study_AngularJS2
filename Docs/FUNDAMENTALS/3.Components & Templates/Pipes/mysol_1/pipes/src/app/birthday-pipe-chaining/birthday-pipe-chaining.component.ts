6dad29579f5560d6ad23d7fc37a7c7ebdb61090b
specter01wj <specter01wj@gmail.com>
2020-05-11 21:37:32 -0600

    05/10/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/birthday-pipe-chaining/birthday-pipe-chaining.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/birthday-pipe-chaining/birthday-pipe-chaining.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./birthday-pipe-chaining.component.less']
 })
 export class BirthdayPipeChainingComponent implements OnInit {
-
+	birthday = new Date(1988, 3, 15); // April 15, 1988
+	
   constructor() { }
 
   ngOnInit(): void {
