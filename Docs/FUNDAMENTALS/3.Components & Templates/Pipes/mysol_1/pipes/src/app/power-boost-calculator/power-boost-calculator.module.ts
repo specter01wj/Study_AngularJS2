16e07f3ca5c362b8e8ed0331b1da41c3b042d157
specter01wj <specter01wj@gmail.com>
2020-05-12 15:32:39 -0600

    05/10/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/power-boost-calculator/power-boost-calculator.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/power-boost-calculator/power-boost-calculator.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { PowerBoostCalculatorRoutingModule } from './power-boost-calculator-routing.module';
+
 import { PowerBoostCalculatorComponent } from './power-boost-calculator.component';
 
-
-
 @NgModule({
-  declarations: [PowerBoostCalculatorComponent],
+  declarations: [
+  	PowerBoostCalculatorComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    PowerBoostCalculatorRoutingModule
+  ],
+  exports: []
 })
 export class PowerBoostCalculatorModule { }
