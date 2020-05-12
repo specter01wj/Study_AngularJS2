ee8577f25dcee192abb9ef6ca3efdbc7a9c7acce
specter01wj <specter01wj@gmail.com>
2020-05-11 21:36:29 -0600

    05/10/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/birthday-pipe-chaining/birthday-pipe-chaining-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/birthday-pipe-chaining/birthday-pipe-chaining-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+import { BirthdayPipeChainingComponent } from './birthday-pipe-chaining.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: BirthdayPipeChainingComponent,
+    children: [
+    ]
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class BirthdayPipeChainingRoutingModule { }
