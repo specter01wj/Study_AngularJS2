95c7c2ba90e950ad58340d0c62787c1191ecd105
specter01wj <specter01wj@gmail.com>
2020-05-11 22:07:48 -0600

    05/10/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/power-booster/power-booster-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/power-booster/power-booster-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+import { PowerBoosterComponent } from './power-booster.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: PowerBoosterComponent,
+    children: [
+    ]
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class PowerBoosterRoutingModule { }
