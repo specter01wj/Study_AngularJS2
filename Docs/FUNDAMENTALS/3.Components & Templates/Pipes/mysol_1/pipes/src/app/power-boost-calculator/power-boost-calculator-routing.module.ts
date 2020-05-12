54375b20d98bd6093c68bdb5a29a098e9497fadd
specter01wj <specter01wj@gmail.com>
2020-05-12 16:37:59 -0600

    05/10/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/power-boost-calculator/power-boost-calculator-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/power-boost-calculator/power-boost-calculator-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+import { PowerBoostCalculatorComponent } from './power-boost-calculator.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: PowerBoostCalculatorComponent,
+    children: [
+    ]
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class PowerBoostCalculatorRoutingModule { }
