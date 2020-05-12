5c74edc5424267ad436f00fec6d6fe17152d307c
specter01wj <specter01wj@gmail.com>
2020-05-11 22:06:54 -0600

    05/10/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/power-booster/power-booster.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/power-booster/power-booster.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { PowerBoosterRoutingModule } from './power-booster-routing.module';
+
 import { PowerBoosterComponent } from './power-booster.component';
 
-
-
 @NgModule({
-  declarations: [PowerBoosterComponent],
+  declarations: [
+  	PowerBoosterComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    PowerBoosterRoutingModule
+  ],
+  exports: []
 })
 export class PowerBoosterModule { }
