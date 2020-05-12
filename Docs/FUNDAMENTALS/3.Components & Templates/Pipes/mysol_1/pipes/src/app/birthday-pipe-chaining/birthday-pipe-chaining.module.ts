80733b9badbd6410229994deb736e8d466ccc844
specter01wj <specter01wj@gmail.com>
2020-05-11 21:31:22 -0600

    05/10/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/birthday-pipe-chaining/birthday-pipe-chaining.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/birthday-pipe-chaining/birthday-pipe-chaining.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { BirthdayPipeChainingRoutingModule } from './birthday-pipe-chaining-routing.module';
+
 import { BirthdayPipeChainingComponent } from './birthday-pipe-chaining.component';
 
-
-
 @NgModule({
-  declarations: [BirthdayPipeChainingComponent],
+  declarations: [
+  	BirthdayPipeChainingComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    BirthdayPipeChainingRoutingModule
+  ],
+  exports: []
 })
 export class BirthdayPipeChainingModule { }
