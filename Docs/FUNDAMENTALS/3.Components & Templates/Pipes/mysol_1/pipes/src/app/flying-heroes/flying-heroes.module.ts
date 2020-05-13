591cdcd1dd34b31e47509c52ec383cb676fafeb3
specter01wj <specter01wj@gmail.com>
2020-05-12 22:56:58 -0600

    05/10/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/flying-heroes/flying-heroes.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/flying-heroes/flying-heroes.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { FlyingHeroesRoutingModule } from './flying-heroes-routing.module';
+
 import { FlyingHeroesComponent } from './flying-heroes.component';
 
-
-
 @NgModule({
-  declarations: [FlyingHeroesComponent],
+  declarations: [
+  	FlyingHeroesComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    FlyingHeroesRoutingModule
+  ],
+  exports: []
 })
 export class FlyingHeroesModule { }
