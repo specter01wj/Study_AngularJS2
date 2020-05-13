d991d425ba55cab0789735c82e929bfc3a8f5656
specter01wj <specter01wj@gmail.com>
2020-05-12 23:02:41 -0600

    05/10/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/flying-heroes/flying-heroes.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/flying-heroes/flying-heroes.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HEROES } from '../interfaces/heroes';
+
 @Component({
   selector: 'app-flying-heroes',
   templateUrl: './flying-heroes.component.html',
   styleUrls: ['./flying-heroes.component.less']
 })
 export class FlyingHeroesComponent implements OnInit {
+	heroes: any[] = [];
+  canFly = true;
+  mutate = true;
+  title = 'Flying Heroes (pure pipe)';
 
-  constructor() { }
+  constructor() { 
+  	this.reset();
+  }
 
   ngOnInit(): void {
   }
+
+  addHero(name: string) {
+    name = name.trim();
+    if (!name) { return; }
+    let hero = {name, canFly: this.canFly};
+    if (this.mutate) {
+    // Pure pipe won't update display because heroes array reference is unchanged
+    // Impure pipe will display
+    this.heroes.push(hero);
+    } else {
+      // Pipe updates display because heroes array is a new object
+      this.heroes = this.heroes.concat(hero);
+    }
+  }
+
+  reset() { this.heroes = HEROES.slice(); }
 
 }
