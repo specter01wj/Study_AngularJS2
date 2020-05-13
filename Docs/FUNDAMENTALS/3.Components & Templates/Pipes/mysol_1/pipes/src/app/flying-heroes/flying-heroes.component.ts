917d77f7144bd444f9d35dffd04224957a608ca2
specter01wj <specter01wj@gmail.com>
2020-05-12 23:13:08 -0600

    05/10/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/flying-heroes/flying-heroes.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/flying-heroes/flying-heroes.component.ts
@@ -22,18 +22,22 @@ export class FlyingHeroesComponent implements OnInit {
 
   addHero(name: string) {
     name = name.trim();
-    if (!name) { return; }
+    if (!name) { 
+    	return; 
+    }
     let hero = {name, canFly: this.canFly};
     if (this.mutate) {
     // Pure pipe won't update display because heroes array reference is unchanged
     // Impure pipe will display
-    this.heroes.push(hero);
+    	this.heroes.push(hero);
     } else {
       // Pipe updates display because heroes array is a new object
       this.heroes = this.heroes.concat(hero);
     }
   }
 
-  reset() { this.heroes = HEROES.slice(); }
+  reset() { 
+  	this.heroes = HEROES.slice(); 
+  }
 
 }
