228710c9fc8914b0f42a33ddb43a5e6daab6aabf
specter01wj <specter01wj@gmail.com>
2020-05-13 22:32:31 -0600

    05/13/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/flying-heroes-impure/flying-heroes-impure.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/flying-heroes-impure/flying-heroes-impure.component.ts
@@ -1,20 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FlyingHeroesModule } from '../flying-heroes/flying-heroes.module';
+import { HEROES } from '../interfaces/heroes';
 
 @Component({
   selector: 'app-flying-heroes-impure',
   templateUrl: './flying-heroes-impure.component.html',
   styleUrls: ['./flying-heroes-impure.component.less']
 })
-export class FlyingHeroesImpureComponent extends FlyingHeroesComponent implements OnInit {
+export class FlyingHeroesImpureComponent implements OnInit {
 	title = 'Flying Heroes (impure pipe)';
+	heroes: any[] = [];
+  canFly = true;
+  mutate = true;
 
   constructor() { 
-  	super();
+  	this.reset();
   }
 
   ngOnInit(): void {
+  }
+
+  addHero(name: string) {
+    name = name.trim();
+    if (!name) { 
+    	return; 
+    }
+    let hero = {name, canFly: this.canFly};
+    if (this.mutate) {
+    // Pure pipe won't update display because heroes array reference is unchanged
+    // Impure pipe will display
+    	this.heroes.push(hero);
+    } else {
+      // Pipe updates display because heroes array is a new object
+      this.heroes = this.heroes.concat(hero);
+    }
+  }
+
+  reset() { 
+  	this.heroes = HEROES.slice(); 
   }
 
 }
--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/flying-heroes-impure/flying-heroes-impure.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/flying-heroes-impure/flying-heroes-impure.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { FlyingHeroesModule } from '../flying-heroes/flying-heroes.module';
 import { FlyingHeroesImpureRoutingModule } from './flying-heroes-impure-routing.module';
 
 import { FlyingHeroesImpureComponent } from './flying-heroes-impure.component';
@@ -13,7 +14,8 @@ import { FlyingHeroesImpureComponent } from './flying-heroes-impure.component';
   imports: [
     CommonModule,
     FormsModule,
-    FlyingHeroesImpureRoutingModule
+    FlyingHeroesImpureRoutingModule,
+    FlyingHeroesModule
   ],
   exports: []
 })
