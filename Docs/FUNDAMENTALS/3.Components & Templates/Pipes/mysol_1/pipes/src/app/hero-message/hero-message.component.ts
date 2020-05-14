c0dfe88626cea96c6db67b36d8cc03258c519bc2
specter01wj <specter01wj@gmail.com>
2020-05-13 22:54:50 -0600

    05/13/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/hero-message/hero-message.component.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/hero-message/hero-message.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable, interval } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+
 @Component({
   selector: 'app-hero-message',
   templateUrl: './hero-message.component.html',
   styleUrls: ['./hero-message.component.less']
 })
 export class HeroMessageComponent implements OnInit {
+	message$: Observable<string>;
 
-  constructor() { }
+  private messages = [
+    'You are my hero!',
+    'You are the best hero!',
+    'Will you be my hero?'
+  ];
+
+  constructor() { 
+  	this.resend();
+  }
 
   ngOnInit(): void {
+  }
+
+  resend() {
+    /*this.message$ = interval(500).pipe(
+      map(i => this.messages[i]),
+      take(this.messages.length)
+    );*/
+    this.message$ = fromArray(this.messages).pipe(
+		  map(message => timer(500),
+		  map(() => message)),
+		  concatAll()
+		);
   }
 
 }
