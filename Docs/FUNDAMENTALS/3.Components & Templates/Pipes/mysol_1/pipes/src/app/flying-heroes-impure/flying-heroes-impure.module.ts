a7b4b08ff25b1ef5717e311195ffa6f135f42207
specter01wj <specter01wj@gmail.com>
2020-05-13 22:36:35 -0600

    05/13/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/flying-heroes-impure/flying-heroes-impure.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/flying-heroes-impure/flying-heroes-impure.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { PipesModuleModule } from '../common/pipes-module.module';
 import { FlyingHeroesModule } from '../flying-heroes/flying-heroes.module';
 import { FlyingHeroesImpureRoutingModule } from './flying-heroes-impure-routing.module';
 
@@ -15,7 +16,8 @@ import { FlyingHeroesImpureComponent } from './flying-heroes-impure.component';
     CommonModule,
     FormsModule,
     FlyingHeroesImpureRoutingModule,
-    FlyingHeroesModule
+    FlyingHeroesModule,
+    PipesModuleModule
   ],
   exports: []
 })
