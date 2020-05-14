fc6b557c186cc04b8ff4b57ce8e2dd073da964db
specter01wj <specter01wj@gmail.com>
2020-05-13 23:07:01 -0600

    05/13/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/hero-list/hero-list-routing.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/hero-list/hero-list-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+import { HeroListComponent } from './hero-list.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: HeroListComponent,
+    children: [
+    ]
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class HeroListRoutingModule { }
