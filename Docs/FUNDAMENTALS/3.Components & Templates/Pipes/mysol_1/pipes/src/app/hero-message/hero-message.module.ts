f2356fe201f3ad398a329251641a2c9a2e827c09
specter01wj <specter01wj@gmail.com>
2020-05-13 22:50:54 -0600

    05/13/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/hero-message/hero-message.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Pipes/mysol_1/pipes/src/app/hero-message/hero-message.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { HeroMessageRoutingModule } from './hero-message-routing.module';
+
 import { HeroMessageComponent } from './hero-message.component';
 
-
-
 @NgModule({
-  declarations: [HeroMessageComponent],
+  declarations: [
+  	HeroMessageComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    HeroMessageRoutingModule
+  ],
+  exports: []
 })
 export class HeroMessageModule { }
