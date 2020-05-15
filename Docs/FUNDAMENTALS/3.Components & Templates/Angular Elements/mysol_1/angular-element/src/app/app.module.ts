1279d3e4f883f53a2ce538e4da0f44265f9d492d
specter01wj <specter01wj@gmail.com>
2020-05-14 22:42:29 -0600

    05/13/2020 v1

--- Docs/FUNDAMENTALS/3.Components & Templates/Angular Elements/mysol_1/angular-element/src/app/app.module.ts
+++ Docs/FUNDAMENTALS/3.Components & Templates/Angular Elements/mysol_1/angular-element/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { PopupComponent } from './popup/popup.component';
+
+import { PopupService } from './services/popup.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +16,10 @@ import { PopupComponent } from './popup/popup.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [PopupService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
