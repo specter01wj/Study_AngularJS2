1c828880aba4fae5079c0128f18aa93b2476bddb
specter01wj <specter01wj@gmail.com>
2020-05-27 22:48:02 -0600

    05/24/2020 v1

--- Docs/FUNDAMENTALS/4.Forms for User Input/Reactive Forms/mysol_1/reactive-form/src/app/name-editor/name-editor.component.ts
+++ Docs/FUNDAMENTALS/4.Forms for User Input/Reactive Forms/mysol_1/reactive-form/src/app/name-editor/name-editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-name-editor',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NameEditorComponent implements OnInit {
 	name = new FormControl('');
-	
+
   constructor() { }
 
   ngOnInit(): void {
