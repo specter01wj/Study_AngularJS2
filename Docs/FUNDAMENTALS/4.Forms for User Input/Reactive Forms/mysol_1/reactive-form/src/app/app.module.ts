0cc0fddf54d3b0bedf90a314ac4e1fd76e536cb6
specter01wj <specter01wj@gmail.com>
2020-05-27 22:43:40 -0600

    05/24/2020 v1

--- Docs/FUNDAMENTALS/4.Forms for User Input/Reactive Forms/mysol_1/reactive-form/src/app/app.module.ts
+++ Docs/FUNDAMENTALS/4.Forms for User Input/Reactive Forms/mysol_1/reactive-form/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
+import { NameEditorComponent } from './name-editor/name-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileEditorComponent
+    ProfileEditorComponent,
+    NameEditorComponent
   ],
   imports: [
     BrowserModule,
