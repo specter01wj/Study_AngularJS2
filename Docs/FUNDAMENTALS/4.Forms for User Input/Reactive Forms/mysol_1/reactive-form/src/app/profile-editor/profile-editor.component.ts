1d2e14a9540a17bc0d0b93f6784579930cc6a893
specter01wj <specter01wj@gmail.com>
2020-05-27 22:49:05 -0600

    05/24/2020 v1

--- Docs/FUNDAMENTALS/4.Forms for User Input/Reactive Forms/mysol_1/reactive-form/src/app/profile-editor/profile-editor.component.ts
+++ Docs/FUNDAMENTALS/4.Forms for User Input/Reactive Forms/mysol_1/reactive-form/src/app/profile-editor/profile-editor.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
