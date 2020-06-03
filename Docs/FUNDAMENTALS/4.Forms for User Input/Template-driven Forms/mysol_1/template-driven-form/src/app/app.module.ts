9a20097e5d5ade8168b41345011aa6c647010d19
specter01wj <specter01wj@gmail.com>
2020-06-02 23:50:45 -0600

    06/03/2020 v1

--- Docs/FUNDAMENTALS/4.Forms for User Input/Template-driven Forms/mysol_1/template-driven-form/src/app/app.module.ts
+++ Docs/FUNDAMENTALS/4.Forms for User Input/Template-driven Forms/mysol_1/template-driven-form/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ Docs/FUNDAMENTALS/4.Forms for User Input/Template-driven Forms/mysol_1/template-driven-form/src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.less']
+})
+export class HeroFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
