48741329059bb2b5ce00fd8e0531fcde3d7bad0d
specter01wj <specter01wj@gmail.com>
2020-06-03 23:05:05 -0600

    06/03/2020 v1

--- Docs/FUNDAMENTALS/4.Forms for User Input/Template-driven Forms/mysol_1/template-driven-form/src/app/hero-form/hero-form.component.ts
+++ Docs/FUNDAMENTALS/4.Forms for User Input/Template-driven Forms/mysol_1/template-driven-form/src/app/hero-form/hero-form.component.ts
@@ -30,7 +30,8 @@ export class HeroFormComponent implements OnInit {
   }
 
   skyDog(): Hero {
-    let myHero =  new Hero(42, 'SkyDog',
+    let myHero =  new Hero(42, 
+                           'SkyDog',
                            'Fetch any object at any distance',
                            'Leslie Rollover');
     console.log('My hero is called ' + myHero.name); // "My hero is called SkyDog"
