19d33d36ad66b5171a280efaddf8b0387f096326
specter01wj <specter01wj@gmail.com>
2020-06-07 22:59:24 -0600

    06/04/2020 v1

--- Docs/FUNDAMENTALS/4.Forms for User Input/Validate form input/mysol_1/validate-form-input/src/app/shared/alter-ego.directive.ts
+++ Docs/FUNDAMENTALS/4.Forms for User Input/Validate form input/mysol_1/validate-form-input/src/app/shared/alter-ego.directive.ts
@@ -1,10 +1,40 @@
-import { Directive } from '@angular/core';
+import { Directive, forwardRef, Injectable } from '@angular/core';
+import { AsyncValidator, AbstractControl, NG_ASYNC_VALIDATORS, ValidationErrors } from '@angular/forms';
+import { catchError, map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+
+import { HeroesService } from './heroes.service';
 
 @Directive({
-  selector: '[appAlterEgo]'
+  selector: '[appAlterEgo]',
+  providers: [
+    {
+      provide: NG_ASYNC_VALIDATORS,
+      useExisting: forwardRef(() => UniqueAlterEgoValidator),
+      multi: true
+    }
+  ]
 })
 export class AlterEgoDirective {
 
-  constructor() { }
+  constructor(private validator: UniqueAlterEgoValidator) { }
 
+  validate(control: AbstractControl) {
+    this.validator.validate(control);
+  }
+
+}
+
+@Injectable({ providedIn: 'root' })
+export class UniqueAlterEgoValidator implements AsyncValidator {
+  constructor(private heroesService: HeroesService) {}
+
+  validate(
+    ctrl: AbstractControl
+  ): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    return this.heroesService.isAlterEgoTaken(ctrl.value).pipe(
+      map(isTaken => (isTaken ? { uniqueAlterEgo: true } : null)),
+      catchError(() => of(null))
+    );
+  }
 }
