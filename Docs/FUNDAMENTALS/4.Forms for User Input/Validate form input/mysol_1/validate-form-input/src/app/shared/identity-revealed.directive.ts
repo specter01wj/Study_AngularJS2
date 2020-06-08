72f51e59ef53e5ae0a4f1ec3a07231339db6e2e0
specter01wj <specter01wj@gmail.com>
2020-06-07 23:04:35 -0600

    06/04/2020 v1

--- Docs/FUNDAMENTALS/4.Forms for User Input/Validate form input/mysol_1/validate-form-input/src/app/shared/identity-revealed.directive.ts
+++ Docs/FUNDAMENTALS/4.Forms for User Input/Validate form input/mysol_1/validate-form-input/src/app/shared/identity-revealed.directive.ts
@@ -1,10 +1,23 @@
 import { Directive } from '@angular/core';
+import { AbstractControl, FormGroup, NG_VALIDATORS, ValidationErrors, Validator, ValidatorFn } from '@angular/forms';
+
+export const identityRevealedValidator: ValidatorFn = (control: FormGroup): ValidationErrors | null => {
+  const name = control.get('name');
+  const alterEgo = control.get('alterEgo');
+
+  return name && alterEgo && name.value === alterEgo.value ? { 'identityRevealed': true } : null;
+};
 
 @Directive({
-  selector: '[appIdentityRevealed]'
+  selector: '[appIdentityRevealed]',
+  providers: [{ provide: NG_VALIDATORS, useExisting: IdentityRevealedDirective, multi: true }]
 })
-export class IdentityRevealedDirective {
+export class IdentityRevealedDirective implements Validator {
 
   constructor() { }
+
+  validate(control: AbstractControl): ValidationErrors {
+    return identityRevealedValidator(control)
+  }
 
 }
