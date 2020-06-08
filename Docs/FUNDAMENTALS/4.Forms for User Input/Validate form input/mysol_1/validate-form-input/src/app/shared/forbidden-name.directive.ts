0aa75db5170a510ea84c8c8d5d69fdd307b91d36
specter01wj <specter01wj@gmail.com>
2020-06-07 23:02:43 -0600

    06/04/2020 v1

--- Docs/FUNDAMENTALS/4.Forms for User Input/Validate form input/mysol_1/validate-form-input/src/app/shared/forbidden-name.directive.ts
+++ Docs/FUNDAMENTALS/4.Forms for User Input/Validate form input/mysol_1/validate-form-input/src/app/shared/forbidden-name.directive.ts
@@ -1,10 +1,31 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, Validator, ValidatorFn, Validators } from '@angular/forms';
+
+export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+  return (control: AbstractControl): {[key: string]: any} | null => {
+    const forbidden = nameRe.test(control.value);
+    return forbidden ? {'forbiddenName': {value: control.value}} : null;
+  };
+}
 
 @Directive({
-  selector: '[appForbiddenName]'
+  selector: '[appForbiddenName]',
+  providers: [
+  	{
+  		provide: NG_VALIDATORS, 
+  		useExisting: ForbiddenNameDirective, 
+  		multi: true
+  	}
+  ]
 })
-export class ForbiddenNameDirective {
+export class ForbiddenNameDirective implements Validator {
+	@Input('appForbiddenName') forbiddenName: string;
 
   constructor() { }
+  
+  validate(control: AbstractControl): {[key: string]: any} | null {
+    return this.forbiddenName ? forbiddenNameValidator(new RegExp(this.forbiddenName, 'i'))(control)
+                              : null;
+  }
 
 }
