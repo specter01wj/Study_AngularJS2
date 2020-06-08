8f7cb7157730c21d8c6d1e4ce3cb6d5f870a4be9
specter01wj <specter01wj@gmail.com>
2020-06-08 15:26:08 -0600

    06/04/2020 v1

--- Docs/FUNDAMENTALS/4.Forms for User Input/Building Dynamic Forms/mysol_1/dynamic-forms/src/app/app.module.ts
+++ Docs/FUNDAMENTALS/4.Forms for User Input/Building Dynamic Forms/mysol_1/dynamic-forms/src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule }          from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { DynamicFormQuestionComponent } from './dynamic-form-question/dynamic-form-question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DynamicFormComponent,
+    DynamicFormQuestionComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
