569d1d0542ba45f421e938402d8f218ab908ff19
specter01wj <specter01wj@gmail.com>
2020-07-13 23:51:19 -0600

    06/04/2020 v1

---
+++ Docs/FUNDAMENTALS/6.NgModules/Feature Modules/mysol_1/feature-module/src/app/customer-dashboard/customer-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-dashboard',
+  templateUrl: './customer-dashboard.component.html',
+  styleUrls: ['./customer-dashboard.component.less']
+})
+export class CustomerDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Docs/FUNDAMENTALS/6.NgModules/Feature Modules/mysol_1/feature-module/src/app/customer-dashboard/customer-dashboard.module.ts
+++ Docs/FUNDAMENTALS/6.NgModules/Feature Modules/mysol_1/feature-module/src/app/customer-dashboard/customer-dashboard.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CustomerDashboardComponent } from './customer-dashboard.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CustomerDashboardComponent],
   imports: [
     CommonModule
   ]
