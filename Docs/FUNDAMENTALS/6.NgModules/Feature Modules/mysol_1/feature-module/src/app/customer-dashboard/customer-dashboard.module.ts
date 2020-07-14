246002658bb8ef2189d89142d8410cc1f192c43f
specter01wj <specter01wj@gmail.com>
2020-07-13 23:54:49 -0600

    06/04/2020 v1

--- Docs/FUNDAMENTALS/6.NgModules/Feature Modules/mysol_1/feature-module/src/app/customer-dashboard/customer-dashboard.module.ts
+++ Docs/FUNDAMENTALS/6.NgModules/Feature Modules/mysol_1/feature-module/src/app/customer-dashboard/customer-dashboard.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { CustomerDashboardComponent } from './customer-dashboard.component';
 
-
-
 @NgModule({
-  declarations: [CustomerDashboardComponent],
+  declarations: [
+  	CustomerDashboardComponent
+  ],
   imports: [
     CommonModule
   ]
