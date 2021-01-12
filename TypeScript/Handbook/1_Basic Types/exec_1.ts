5f61ec39b01fa3a592a859a4a86bde71cc691778
Jin Wang <specter01wj@gmail.com>
2021-01-12 18:18:04 -0500

    01/01/2021 v1

--- TypeScript/Handbook/1_Basic Types/exec_1.ts
+++ TypeScript/Handbook/1_Basic Types/exec_1.ts
@@ -2,10 +2,12 @@
 
 namespace demo_01_01 {
 
+	// Boolean
 	let isDone: boolean = false;
 	console.log(isDone);
 
 
+	// Number
 	let decimal: number = 6;
 	let hex: number = 0xf00d;
 	let binary: number = 0b1010;
@@ -15,6 +17,7 @@ namespace demo_01_01 {
 	// -t es2020
 
 
+	// String
 	let color: string = 'blue';
 	color = 'red';
 
@@ -24,6 +27,30 @@ namespace demo_01_01 {
 
 	I'll be ${age + 1} years old next month.`
 	console.log(sentence);
+
+	let sentence2: string =
+	  "Hello, my name is " +
+	  fullName +
+	  ".\n\n" +
+	  "I'll be " +
+	  (age + 1) +
+	  " years old next month.";
+
+
+
+	  // Array
+	  let list: number[] = [1, 2, 3];
+	  let list2: Array<number> = [1, 2, 3];
+
+
+
+	  // Tuple
+	  let x: [string, number];
+	  x = ["hello", 10];
+	  console.log(x);
+
+
+
 
 
 
