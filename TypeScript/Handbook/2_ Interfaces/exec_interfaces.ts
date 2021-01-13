04d94852b64028bb7465e73fa78a78536020b450
Jin Wang <specter01wj@gmail.com>
2021-01-13 11:06:16 -0500

    01/01/2021 v1

--- TypeScript/Handbook/2_ Interfaces/exec_interfaces.ts
+++ TypeScript/Handbook/2_ Interfaces/exec_interfaces.ts
@@ -57,6 +57,17 @@ namespace demo_02_01 {
 	let p1: Point = { x: 10, y: 20};
 	// p1.x = 5;
 
+	let a: number[] = [1, 2, 3, 4];
+	let ro: ReadonlyArray<number> = a;
+
+	// ro[0] = 12;
+	// ro.push(5);
+	// ro.length = 100;
+	// a = ro;
+	a = ro as number[];
+
+
+
 	// Excess Property Checks
 
 
