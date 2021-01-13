11079eef0bf6bb6b92ef1e96f5b36d669cd24e0b
Jin Wang <specter01wj@gmail.com>
2021-01-13 10:51:32 -0500

    01/01/2021 v1

--- TypeScript/Handbook/2_ Interfaces/exec_interfaces.ts
+++ TypeScript/Handbook/2_ Interfaces/exec_interfaces.ts
@@ -25,8 +25,23 @@ namespace demo_02_01 {
 
 
 	// Optional Properties
+	interface SquareConfig {
+		color?: string;
+		width?: number;
+	}
 
+	function createSquare(config: SquareConfig): {color: string; area: number} {
+		let newSquare = { color: 'white'; area: 100 };
 
+		if (config.color) {
+	    newSquare.color = config.color;
+	  }
+	  if (config.width) {
+	    newSquare.area = config.width * config.width;
+	  }
+
+		return newSquare;
+	}
 
 
 
