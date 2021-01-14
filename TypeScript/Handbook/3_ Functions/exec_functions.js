c38f58b16166ddbadecfb478d1098416f2498b29
Jin Wang <specter01wj@gmail.com>
2021-01-14 16:10:36 -0500

    01/01/2021 v1

--- TypeScript/Handbook/3_ Functions/exec_functions.js
+++ TypeScript/Handbook/3_ Functions/exec_functions.js
@@ -170,4 +170,27 @@ var demo_03_01;
       };
     }*/
     // Overloads
+    var suits = ["hearts", "spades", "clubs", "diamonds"];
+    function pickCard(x) {
+        // Check to see if we're working with an object/array
+        // if so, they gave us the deck and we'll pick the card
+        if (typeof x == "object") {
+            var pickedCard_1 = Math.floor(Math.random() * x.length);
+            return pickedCard_1;
+        }
+        // Otherwise just let them pick the card
+        else if (typeof x == "number") {
+            var pickedSuit = Math.floor(x / 13);
+            return { suit: suits[pickedSuit], card: x % 13 };
+        }
+    }
+    var myDeck = [
+        { suit: "diamonds", card: 2 },
+        { suit: "spades", card: 10 },
+        { suit: "hearts", card: 4 },
+    ];
+    var pickedCard3 = myDeck[pickCard(myDeck)];
+    console.log("card: " + pickedCard3.card + " of " + pickedCard3.suit);
+    var pickedCard4 = pickCard(15);
+    console.log("card: " + pickedCard4.card + " of " + pickedCard4.suit);
 })(demo_03_01 || (demo_03_01 = {}));
--- TypeScript/Handbook/3_ Functions/exec_functions.ts
+++ TypeScript/Handbook/3_ Functions/exec_functions.ts
@@ -230,7 +230,29 @@ namespace demo_03_01 {
 
 
 	// Overloads
+	let suits = ["hearts", "spades", "clubs", "diamonds"];
 
+	function pickCard(x: any): any {
+	  if (typeof x == "object") {
+	    let pickedCard = Math.floor(Math.random() * x.length);
+	    return pickedCard;
+	  } else if (typeof x == "number") {
+	    let pickedSuit = Math.floor(x / 13);
+	    return { suit: suits[pickedSuit], card: x % 13 };
+	  }
+	}
+
+	let myDeck = [
+	  { suit: "diamonds", card: 2 },
+	  { suit: "spades", card: 10 },
+	  { suit: "hearts", card: 4 },
+	];
+
+	let pickedCard3 = myDeck[pickCard(myDeck)];
+	console.log("card: " + pickedCard3.card + " of " + pickedCard3.suit);
+
+	let pickedCard4 = pickCard(15);
+	console.log("card: " + pickedCard4.card + " of " + pickedCard4.suit);
 
 
 
