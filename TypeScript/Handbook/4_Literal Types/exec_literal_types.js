c5c9914dce17ebc9f8bfaba4f66a29a099f9d765
Jin Wang <specter01wj@gmail.com>
2021-01-14 17:15:08 -0500

    01/01/2021 v1

---
+++ TypeScript/Handbook/4_Literal Types/exec_literal_types.js
@@ -0,0 +1,36 @@
+var demo_03_01;
+(function (demo_03_01) {
+    // Literal Narrowing
+    var helloWorld = "Hello World";
+    var hiWorld = "Hi World";
+    var UIElement = /** @class */ (function () {
+        function UIElement() {
+        }
+        UIElement.prototype.animate = function (dx, dy, easing) {
+            if (easing === "ease-in") {
+                console.log('ease-in');
+            }
+            else if (easing === "ease-out") {
+                console.log('ease-out');
+            }
+            else if (easing === "ease-in-out") {
+                console.log('ease-in-out');
+            }
+            else {
+                console.log('error!');
+            }
+        };
+        return UIElement;
+    }());
+    var button = new UIElement();
+    button.animate(0, 0, "ease-in");
+    // button.animate(0, 0, "uneasy");
+    // Numeric Literal Types
+    function rollDice() {
+        return (Math.floor(Math.random() * 6) + 1);
+    }
+    var result = rollDice();
+    console.log(result);
+    // Boolean Literal Types
+    // 
+})(demo_03_01 || (demo_03_01 = {}));
