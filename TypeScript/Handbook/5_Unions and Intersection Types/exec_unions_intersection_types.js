4d2758e5298144e666bf5768be06145071d44977
Jin Wang <specter01wj@gmail.com>
2021-01-15 12:46:34 -0500

    01/01/2021 v1

---
+++ TypeScript/Handbook/5_Unions and Intersection Types/exec_unions_intersection_types.js
@@ -0,0 +1,24 @@
+var demo_05_01;
+(function (demo_05_01) {
+    // Union Types
+    function padLeft(value, padding) {
+        if (typeof padding === "number") {
+            // return Array(padding + 1).join(" ") + value;
+            return (padding + 1) + ' ' + value;
+        }
+        if (typeof padding === "string") {
+            return padding + value;
+        }
+        throw new Error("Expected string or number, got '" + typeof padding + "'.");
+    }
+    var res1 = padLeft("Hello world", 4);
+    var res2 = padLeft("Hello world", "Red");
+    console.log(res1);
+    console.log(res2);
+    // let indentedString = padLeft("Hello world", true);
+    // console.log(indentedString);
+    // Unions with Common Fields
+    // Discriminating Unions
+    // Union Exhaustiveness checking
+    // Intersection Types
+})(demo_05_01 || (demo_05_01 = {}));
--- TypeScript/Handbook/5_Unions and Intersection Types/exec_unions_intersection_types.ts
+++ TypeScript/Handbook/5_Unions and Intersection Types/exec_unions_intersection_types.ts
@@ -5,7 +5,8 @@ namespace demo_05_01 {
 	// Union Types
 	function padLeft(value: string, padding: any) {
 	  if (typeof padding === "number") {
-	    return Array(padding + 1).join(" ") + value;
+	    // return Array(padding + 1).join(" ") + value;
+	    return (padding + 1) + ' ' + value;
 	  }
 	  if (typeof padding === "string") {
 	    return padding + value;
@@ -18,6 +19,20 @@ namespace demo_05_01 {
 	console.log(res1);
 	console.log(res2);
 
+	// let indentedString = padLeft("Hello world", true);
+	// console.log(indentedString);
+
+	function padLeft2(value: string, padding: string | number) {
+	  if (typeof padding === "number") {
+	    return Array(padding + 1).join(" ") + value;
+	  }
+	  if (typeof padding === "string") {
+	    return padding + value;
+	  }
+	  throw new Error(`Expected string or number, got '${typeof padding}'.`);
+	}
+
+	// let indentedString2 = padLeft2("Hello world", true);
 
 
 
