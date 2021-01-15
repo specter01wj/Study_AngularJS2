edf5bb8962f3ce2a41d32d8b51f4d06e14cdb0a0
Jin Wang <specter01wj@gmail.com>
2021-01-15 12:39:49 -0500

    01/01/2021 v1

--- TypeScript/Handbook/5_Unions and Intersection Types/exec_unions_intersection_types.ts
+++ TypeScript/Handbook/5_Unions and Intersection Types/exec_unions_intersection_types.ts
@@ -3,7 +3,20 @@
 namespace demo_05_01 {
 
 	// Union Types
+	function padLeft(value: string, padding: any) {
+	  if (typeof padding === "number") {
+	    return Array(padding + 1).join(" ") + value;
+	  }
+	  if (typeof padding === "string") {
+	    return padding + value;
+	  }
+	  throw new Error(`Expected string or number, got '${typeof padding}'.`);
+	}
 
+	let res1 = padLeft("Hello world", 4);
+	let res2 = padLeft("Hello world", "Red");
+	console.log(res1);
+	console.log(res2);
 
 
 
