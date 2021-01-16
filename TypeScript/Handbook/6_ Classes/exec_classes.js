13d93d6193fbd8d2de338def93367424d2a4733a
Jin Wang <specter01wj@gmail.com>
2021-01-16 11:39:36 -0500

    01/01/2021 v1

--- TypeScript/Handbook/6_ Classes/exec_classes.js
+++ TypeScript/Handbook/6_ Classes/exec_classes.js
@@ -203,7 +203,25 @@ var demo_06_01;
     if (employee5.fullName) {
         console.log(employee5.fullName);
     }
+    // employee5.fullName = 'Giannis Antetokounmpo';
+    // console.log(employee5.fullName);
     // Static Properties
+    class Grid {
+        constructor(scale) {
+            this.scale = scale;
+            // code...
+        }
+        calculateDistanceFromOrigin(point) {
+            let xDist = point.x - Grid.origin.x;
+            let yDist = point.y - Grid.origin.y;
+            return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+        }
+    }
+    Grid.origin = { x: 0, y: 0 };
+    let grid1 = new Grid(1.0);
+    let grid2 = new Grid(5.0);
+    console.log(grid1.calculateDistanceFromOrigin({ x: 10, y: 10 }));
+    console.log(grid2.calculateDistanceFromOrigin({ x: 20, y: 30 }));
     // Abstract Classes
     // Advanced Techniques
     // Constructor functions
--- TypeScript/Handbook/6_ Classes/exec_classes.ts
+++ TypeScript/Handbook/6_ Classes/exec_classes.ts
@@ -285,13 +285,32 @@ namespace demo_06_01 {
 	  console.log(employee5.fullName);
 	}
 
+	// employee5.fullName = 'Giannis Antetokounmpo';
+	// console.log(employee5.fullName);
 
 
+	
 	// Static Properties
+	class Grid {
+		static origin = {x: 0, y: 0};
+		
+		constructor(public scale: number) {
+			// code...
+		}
 
+		calculateDistanceFromOrigin(point: {x: number; y: number}) {
+			let xDist = point.x - Grid.origin.x;
+	    let yDist = point.y - Grid.origin.y;
+	    return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+		}
 
+	}
 
+	let grid1 = new Grid(1.0);
+	let grid2 = new Grid(5.0);
 
+	console.log(grid1.calculateDistanceFromOrigin({ x: 10, y: 10 }));
+	console.log(grid2.calculateDistanceFromOrigin({ x: 20, y: 30 }));
 
 
 
