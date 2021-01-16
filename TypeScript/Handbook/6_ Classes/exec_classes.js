4fafef36fd124fa26f9671060b7d27629c26afe4
Jin Wang <specter01wj@gmail.com>
2021-01-15 20:36:31 -0500

    01/01/2021 v1

--- TypeScript/Handbook/6_ Classes/exec_classes.js
+++ TypeScript/Handbook/6_ Classes/exec_classes.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var demo_06_01;
 (function (demo_06_01) {
     // Classes
@@ -13,6 +26,29 @@ var demo_06_01;
     var greeter = new Greeter('world');
     console.log(greeter.greet());
     // Inheritance
+    var Animal = /** @class */ (function () {
+        function Animal() {
+        }
+        Animal.prototype.move = function (distanceInMeters) {
+            if (distanceInMeters === void 0) { distanceInMeters = 0; }
+            console.log("Animal moved " + distanceInMeters + "m.");
+        };
+        return Animal;
+    }());
+    var Dog = /** @class */ (function (_super) {
+        __extends(Dog, _super);
+        function Dog() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        Dog.prototype.bark = function () {
+            console.log("Woof! Woof!");
+        };
+        return Dog;
+    }(Animal));
+    var dog = new Dog();
+    dog.bark();
+    dog.move(10);
+    dog.bark();
     // Public by default
     // ECMAScript Private Fields
     // Understanding TypeScript’s private
--- TypeScript/Handbook/6_ Classes/exec_classes.ts
+++ TypeScript/Handbook/6_ Classes/exec_classes.ts
@@ -19,10 +19,30 @@ namespace demo_06_01 {
 	console.log(greeter.greet());
 
 
+
 	// Inheritance
+	class Animal {
+		
+		move(distanceInMeters: number = 0) {
+			console.log(`Animal moved ${distanceInMeters}m.`);
+		}
+	}
+
+	class Dog extends Animal {
+		
+		bark() {
+			console.log("Woof! Woof!");
+		}
+	}
+
+	const dog = new Dog();
+	dog.bark();
+	dog.move(10);
+	dog.bark();
 
 
 
+	
 
 	// Public by default
 
