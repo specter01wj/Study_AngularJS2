dac541baeeee20a24fdd559317cb920e1799f8f5
Jin Wang <specter01wj@gmail.com>
2021-01-16 18:49:07 -0500

    01/01/2021 v1

--- TypeScript/Handbook/7_ Enums/exec_enums.ts
+++ TypeScript/Handbook/7_ Enums/exec_enums.ts
@@ -3,8 +3,24 @@
 namespace demo_07_01 {
 
 	// Numeric enums
-	
+	enum Direction {
+		Up = 1,
+		Down,
+		Left,
+		Right,
+	}
 
+
+	enum UserResponse {
+		No = 0,
+		Yes = 1,
+	}
+
+	function respond(recipient: string, message: UserResponse): void {
+	  // ...
+	}
+
+	respond("Princess Caroline", UserResponse.Yes);
 
 
 	// String enums
