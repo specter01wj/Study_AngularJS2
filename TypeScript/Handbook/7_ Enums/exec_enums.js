c86d61bd050e3e33e98ed72328ac912953e8f70d
Jin Wang <specter01wj@gmail.com>
2021-01-17 12:37:11 -0500

    01/01/2021 v1

--- TypeScript/Handbook/7_ Enums/exec_enums.js
+++ TypeScript/Handbook/7_ Enums/exec_enums.js
@@ -95,5 +95,23 @@ var demo_07_01;
     }
     console.log(f3(E3));
     // Enums at compile time
+    let LogLevel;
+    (function (LogLevel) {
+        LogLevel[LogLevel["ERROR"] = 0] = "ERROR";
+        LogLevel[LogLevel["WARN"] = 1] = "WARN";
+        LogLevel[LogLevel["INFO"] = 2] = "INFO";
+        LogLevel[LogLevel["DEBUG"] = 3] = "DEBUG";
+    })(LogLevel || (LogLevel = {}));
+    function printImportant(key, message) {
+        const num = LogLevel[key];
+        if (num <= LogLevel.WARN) {
+            console.log("Log level key is:", key);
+            console.log("Log level value is:", num);
+            console.log("Log level message is:", message);
+        }
+    }
+    printImportant("ERROR", "This is a message");
+    printImportant("WARN", "This is a message");
+    printImportant("DEBUG", "This is a message");
     // Ambient enums
 })(demo_07_01 || (demo_07_01 = {}));
