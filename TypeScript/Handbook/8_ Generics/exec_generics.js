88440f02daccb26283b62bd47af5066b53f39a22
Jin Wang <specter01wj@gmail.com>
2021-01-17 15:56:29 -0500

    01/01/2021 v1

---
+++ TypeScript/Handbook/8_ Generics/exec_generics.js
@@ -0,0 +1,17 @@
+var demo_08_01;
+(function (demo_08_01) {
+    // Hello World of Generics
+    function identity(arg) {
+        return arg;
+    }
+    let output1 = identity('myString');
+    console.log(output1);
+    let output2 = identity('myString');
+    console.log(output2);
+    // Working with Generic Type Variables
+    // Generic Types
+    // Generic Classes
+    // Generic Constraints
+    // Using Type Parameters in Generic Constraints
+    // Using Class Types in Generics
+})(demo_08_01 || (demo_08_01 = {}));
---
+++ TypeScript/Handbook/8_ Generics/exec_generics.ts
@@ -0,0 +1,57 @@
+﻿
+
+namespace demo_08_01 {
+
+	// Hello World of Generics
+	function identity<T>(arg: T): T {
+		return arg;
+	}
+
+	let output1 = identity<string>('myString1');
+	console.log(output1);
+	let output2 = identity('myString2');
+	console.log(output2);
+
+
+
+	// Working with Generic Type Variables
+
+
+
+
+
+	// Generic Types
+
+
+
+
+
+	// Generic Classes
+
+
+
+
+	// Generic Constraints
+
+
+
+
+
+	// Using Type Parameters in Generic Constraints
+
+
+
+
+
+
+	// Using Class Types in Generics
+
+
+
+
+
+
+
+}
+
+
