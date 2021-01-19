b9ba131447d536ab8945296ba51e8f92a4ef67e7
Jin Wang <specter01wj@gmail.com>
2021-01-19 18:43:35 -0500

    01/01/2021 v1

--- TypeScript/Handbook Reference/1_Advanced Types/exec_advanced_types.ts
+++ TypeScript/Handbook Reference/1_Advanced Types/exec_advanced_types.ts
@@ -3,7 +3,27 @@
 namespace demo_01_01 {
 
 	// Type Guards and Differentiating Types
-	
+	/*let pet = getSmallPet();
+
+	if ("swim" in pet) {
+	  pet.swim();
+	}
+
+	if (pet.fly) {
+	  pet.fly();
+	}
+
+
+	let pet2 = getSmallPet();
+	let fishPet = pet2 as Fish;
+	let birdPet = pet2 as Bird;
+
+	if (fishPet.swim) {
+	  fishPet.swim();
+	} else if (birdPet.fly) {
+	  birdPet.fly();
+	}*/
+
 
 
 	// User-Defined Type Guards
