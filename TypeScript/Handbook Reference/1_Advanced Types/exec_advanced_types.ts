22ad4d301e36a55f309bf3853b2baccb4a21da55
Jin Wang <specter01wj@gmail.com>
2021-01-21 11:38:45 -0500

    01/20/2021 v1

--- TypeScript/Handbook Reference/1_Advanced Types/exec_advanced_types.ts
+++ TypeScript/Handbook Reference/1_Advanced Types/exec_advanced_types.ts
@@ -27,10 +27,35 @@ namespace demo_01_01 {
 
 
 	// User-Defined Type Guards
+	function isFish(pet: Fish | Bird): pet is Fish {
+	  return (pet as Fish).swim !== undefined;
+	}
+
+	let pet = getSmallPet();
+
+	if (isFish(pet)) {
+	  pet.swim();
+	} else {
+	  pet.fly();
+	}
 
 
+	const zoo: (Fish | Bird)[] = [getSmallPet(), getSmallPet(), getSmallPet()];
+	const underWater1: Fish[] = zoo.filter(isFish);
+
+	const underWater2: Fish[] = zoo.filter<Fish>(isFish);
+	const underWater3: Fish[] = zoo.filter<Fish>(pet => isFish(pet));
 
 
+	function move(pet: Fish | Bird) {
+	  if ("swim" in pet) {
+	    return pet.swim();
+	  }
+	  return pet.fly();
+	}
+
+
+	
 	// typeof type guards
 
 
