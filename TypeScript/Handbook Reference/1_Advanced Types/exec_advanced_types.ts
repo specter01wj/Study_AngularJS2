b7bf3e47596807f5bf347033c3a1cc79957ca12c
Jin Wang <specter01wj@gmail.com>
2021-01-24 12:23:07 -0500

    01/20/2021 v1

--- TypeScript/Handbook Reference/1_Advanced Types/exec_advanced_types.ts
+++ TypeScript/Handbook Reference/1_Advanced Types/exec_advanced_types.ts
@@ -228,7 +228,23 @@ namespace demo_01_01 {
 
 	// Enum Member Types
 	// Polymorphic this types
-	
+	class BasicCalculator {
+	  public constructor(protected value: number = 0) {}
+	  public currentValue(): number {
+	    return this.value;
+	  }
+	  public add(operand: number): this {
+	    this.value += operand;
+	    return this;
+	  }
+	  public multiply(operand: number): this {
+	    this.value *= operand;
+	    return this;
+	  }
+	  // ... other operations go here ...
+	}
+
+	let v = new BasicCalculator(2).multiply(5).add(1).currentValue();
 	
 
 
