19f112eb96285d0c443a51486ef0a5b7651cee3c
Jin Wang <specter01wj@gmail.com>
2021-01-26 11:04:44 -0500

    01/20/2021 v1

--- TypeScript/Handbook Reference/1_Advanced Types/exec_advanced_types.ts
+++ TypeScript/Handbook Reference/1_Advanced Types/exec_advanced_types.ts
@@ -321,10 +321,20 @@ namespace demo_01_01 {
 	}
 
 
+	type Partial<T> = {
+	  [P in keyof T]?: T[P];
+	};
+
+	type Readonly<T> = {
+	  readonly [P in keyof T]: T[P];
+	};
 
 
+	type PersonPartial = Partial<Person>;
+	type ReadonlyPerson = Readonly<Person>;
 
 
+	
 
 
 
