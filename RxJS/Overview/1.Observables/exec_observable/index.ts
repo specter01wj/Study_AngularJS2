b697b58ffe906b8da698e8f3f49b08cc5960c234
Jin Wang <specter01wj@gmail.com>
2021-01-27 18:20:29 -0500

    01/20/2021 v1

--- RxJS/Overview/1.Observables/exec_observable/index.ts
+++ RxJS/Overview/1.Observables/exec_observable/index.ts
@@ -20,9 +20,15 @@ const observable$ = new Observable(subscriber => {
 });
 
 
+console.log('just before subscribe');
 
+observable$.subscribe({
+  next(x) { console.log(`got value ${x}`); },
+  error(err) { console.error(`something wrong occurred: + ${err}`); },
+  complete() { console.log('done'); }
+});
 
-
+console.log('just after subscribe');
 
 
 
