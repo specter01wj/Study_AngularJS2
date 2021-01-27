a6c71bbb5d65f4f912cd29b94344e97dbed33abe
Jin Wang <specter01wj@gmail.com>
2021-01-27 18:03:16 -0500

    01/20/2021 v1

--- RxJS/Overview/exec_overview/index.ts
+++ RxJS/Overview/exec_overview/index.ts
@@ -1,16 +1,22 @@
 import { Observable, of, from, fromEvent, concat, interval, throwError, Subject,
 				 asyncScheduler, asapScheduler, queueScheduler, merge } from 'rxjs';
 import { mergeMap, map, filter, tap, catchError, take, takeUntil,
-				 multicast, refCount, publish, share,
+				 multicast, refCount, publish, share, scan,
 				 publishLast, publishBehavior, publishReplay } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 import { allBooks, allReaders } from './data';
 
 
 
-fromEvent(document, 'click').subscribe(
+/*fromEvent(document, 'click').subscribe(
 	() => console.log('Clicked!'),
-);
+);*/
+
+
+fromEvent(document, 'click')
+  .pipe(scan(count => count + 1, 0))
+  .subscribe(count => console.log(`Clicked ${count} times`));
+
 
 
 
