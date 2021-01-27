1e080458a5ddb28b50db8458971e5ecb37324807
Jin Wang <specter01wj@gmail.com>
2021-01-27 13:03:27 -0500

    01/20/2021 v1

--- TypeScript/Handbook Reference/1_Advanced Types/exec_advanced_types.ts
+++ TypeScript/Handbook Reference/1_Advanced Types/exec_advanced_types.ts
@@ -399,10 +399,42 @@ namespace demo_01_01 {
 
 	// Conditional Types
 	declare function f<T extends boolean>(x: T): T extends true ? string : number;
-	
+
 	let x = f(Math.random() < 0.5);
 
 
+	type TypeName<T> = T extends string
+	  ? "string"
+	  : T extends number
+	  ? "number"
+	  : T extends boolean
+	  ? "boolean"
+	  : T extends undefined
+	  ? "undefined"
+	  : T extends Function
+	  ? "function"
+	  : "object";
+
+	type T0 = TypeName<string>;
+	//   ^ = type T0 = "string"
+	type T1 = TypeName<"a">;
+	//   ^ = type T1 = "string"
+	type T2 = TypeName<true>;
+	//   ^ = type T2 = "boolean"
+	type T3 = TypeName<() => void>;
+	//   ^ = type T3 = "function"
+	type T4 = TypeName<string[]>;
+	//   ^ = type T4 = "object"
+
+
+	
+
+
+
+
+
+
+	
 
 
 }
