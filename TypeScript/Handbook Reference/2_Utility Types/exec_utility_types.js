7a0ac5c4c3a605c3c4b1d8a5c76ae9bbc7b18994
Jin Wang <specter01wj@gmail.com>
2021-01-29 17:45:30 -0500

    01/20/2021 v1

---
+++ TypeScript/Handbook Reference/2_Utility Types/exec_utility_types.js
@@ -0,0 +1,41 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var demo_02_01;
+(function (demo_02_01) {
+    function updateTodo(todo, fieldsToUpdate) {
+        return __assign(__assign({}, todo), fieldsToUpdate);
+    }
+    var todo1 = {
+        title: "organize desk",
+        description: "clear clutter"
+    };
+    var todo2 = updateTodo(todo1, {
+        description: "throw out trash"
+    });
+    console.log(todo1);
+    console.log(todo2);
+    // Readonly<Type>
+    // Record<Keys,Type>
+    // Pick<Type, Keys>
+    // Omit<Type, Keys>
+    // Exclude<Type, ExcludedUnion>
+    // Extract<Type, Union>
+    // NonNullable<Type>
+    // Parameters<Type>
+    // ConstructorParameters<Type>
+    // ReturnType<Type>
+    // InstanceType<Type>
+    // Required<Type>
+    // ThisParameterType<Type>
+    // OmitThisParameter<Type>
+    // ThisType<Type>
+})(demo_02_01 || (demo_02_01 = {}));
--- TypeScript/Handbook Reference/2_Utility Types/exec_utility_types.ts
+++ TypeScript/Handbook Reference/2_Utility Types/exec_utility_types.ts
@@ -3,8 +3,26 @@
 namespace demo_02_01 {
 
 	// Partial<Type>
-	
+	interface Todo {
+	  title: string;
+	  description: string;
+	}
 
+	function updateTodo(todo: Todo, fieldsToUpdate: Partial<Todo>) {
+	  return { ...todo, ...fieldsToUpdate };
+	}
+
+	const todo1 = {
+	  title: "organize desk",
+	  description: "clear clutter",
+	};
+
+	const todo2 = updateTodo(todo1, {
+	  description: "throw out trash",
+	});
+
+	console.log(todo1);
+	console.log(todo2);
 
 
 
