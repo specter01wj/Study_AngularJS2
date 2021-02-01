ad619cb9c9cd12af8e7ca526105eeabb7a98f79c
Jin Wang <specter01wj@gmail.com>
2021-01-31 23:11:27 -0500

    01/20/2021 v1

--- TypeScript/Handbook Reference/2_Utility Types/exec_utility_types.ts
+++ TypeScript/Handbook Reference/2_Utility Types/exec_utility_types.ts
@@ -212,9 +212,15 @@ namespace demo_02_01 {
 	}
 
 
-	
+
 	// OmitThisParameter<Type>
-	
+	function toHex2(this: Number) {
+	  return this.toString(16);
+	}
+
+	const fiveToHex: OmitThisParameter<typeof toHex2> = toHex.bind(5);
+
+	console.log(fiveToHex());
 
 
 
