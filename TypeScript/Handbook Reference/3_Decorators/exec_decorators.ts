cb058e274e48629b735c261adf8f9c3524751168
Jin Wang <specter01wj@gmail.com>
2021-01-31 23:21:28 -0500

    01/20/2021 v1

---
+++ TypeScript/Handbook Reference/3_Decorators/exec_decorators.ts
@@ -0,0 +1,73 @@
+﻿
+
+namespace demo_03_01 {
+
+	// Decorators
+
+
+
+	// Decorator Factories
+
+
+
+
+
+	// Decorator Composition
+
+
+
+
+
+	// Decorator Evaluation
+
+
+	// Class Decorators
+
+
+
+
+
+
+
+
+	// Method Decorators
+
+
+
+
+
+
+
+
+
+	// Accessor Decorators
+
+
+
+
+
+
+
+
+	// Property Decorators
+
+
+
+
+
+
+
+	// Parameter Decorators
+
+
+
+
+
+
+	// Metadata
+	
+
+
+}
+
+
