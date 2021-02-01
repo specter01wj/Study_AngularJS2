d3d1385fb03815d0fd0d654e0585d98cfdf7a6b6
Jin Wang <specter01wj@gmail.com>
2021-01-31 22:47:49 -0500

    01/20/2021 v1

--- TypeScript/Handbook Reference/2_Utility Types/exec_utility_types.ts
+++ TypeScript/Handbook Reference/2_Utility Types/exec_utility_types.ts
@@ -116,9 +116,23 @@ namespace demo_02_01 {
 
 
 	// Parameters<Type>
+	declare function f1(arg: { a: number; b: string }): void;
+
+	type T7 = Parameters<() => string>;
 	
+	type T8 = Parameters<(s: string) => void>;
+	
+	type T9 = Parameters<<T>(arg: T) => T>;
+	
+	type T10 = Parameters<typeof f1>;
 
-
+	type T11 = Parameters<any>;
+	
+	type T12 = Parameters<never>;
+	
+	// type T13 = Parameters<string>;
+	
+	// type T14 = Parameters<Function>;
 
 
 
