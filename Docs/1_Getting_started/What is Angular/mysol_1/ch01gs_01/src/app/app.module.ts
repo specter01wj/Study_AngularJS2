3ff740805c2d8619f7f38e330926f63d8aa2e07c
Jin Wang <specter01wj@gmail.com>
2023-03-01 17:14:49 -0500

    v5

--- Docs/1_Getting_started/What is Angular/mysol_1/ch01gs_01/src/app/app.module.ts
+++ Docs/1_Getting_started/What is Angular/mysol_1/ch01gs_01/src/app/app.module.ts
@@ -4,9 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { HelloWorldTemplateComponent } from './hello-world-template.component';
+import { HelloWorldNgIfComponent } from './hello-world-ngif/hello-world-ngif.component';
+import { HelloWorldDependencyInjectionComponent } from './hello-world-di/hello-world-di.component';
+import { HelloWorldInterpolationComponent } from './hello-world-interpolation/hello-world-interpolation.component';
+import { HelloWorldBindingsComponent } from './hello-world-bindings/hello-world-bindings.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    HelloWorldTemplateComponent,
+    HelloWorldNgIfComponent,
+    HelloWorldDependencyInjectionComponent,
+    HelloWorldInterpolationComponent,
+    HelloWorldBindingsComponent
   ],
   imports: [
     BrowserModule,
