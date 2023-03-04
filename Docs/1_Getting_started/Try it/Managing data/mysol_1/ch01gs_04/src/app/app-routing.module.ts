7224f774f29b933c1a78b784b031aa9dc04b5869
Jin Wang <specter01wj@gmail.com>
2023-03-03 19:15:01 -0500

    v10

--- Docs/1_Getting_started/Try it/Managing data/mysol_1/ch01gs_04/src/app/app-routing.module.ts
+++ Docs/1_Getting_started/Try it/Managing data/mysol_1/ch01gs_04/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'products/:productId', component: ProductDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Docs/1_Getting_started/Try it/Managing data/mysol_1/ch01gs_04/src/app/app.module.ts
+++ Docs/1_Getting_started/Try it/Managing data/mysol_1/ch01gs_04/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
