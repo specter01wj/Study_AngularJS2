4a8460f72807644e25e750c3444ee5dd99aa1bae
specter01wj <specter01wj@gmail.com>
2023-07-23 14:02:13 -0400

    v8

--- Docs/1_Getting_started/Try_it/3_Managing_data/mysol_1/ch01gs_04/src/app/shipping/shipping.component.ts
+++ Docs/1_Getting_started/Try_it/3_Managing_data/mysol_1/ch01gs_04/src/app/shipping/shipping.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { CartService } from '../service/cart.service';
 
 @Component({
   selector: 'app-shipping',
   templateUrl: './shipping.component.html',
   styleUrls: ['./shipping.component.scss']
 })
-export class ShippingComponent {
+export class ShippingComponent implements OnInit {
+
+  shippingCosts!: Observable<{ type: string, price: number }[]>;
+
+  constructor(private cartService: CartService) { }
+
+  ngOnInit(): void {
+    this.shippingCosts =  this.cartService.getShippingPrices();
+  }
+
+  
 
 }
