d6b937225e1dd9d5f26d6a8338bb2107f5a052c9
specter01wj <specter01wj@gmail.com>
2023-07-23 17:40:18 -0400

    v1

---
+++ Docs/1_Getting_started/Try_it/4_Using_forms_for_user_input/mysol_1/ch01gs_04/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Docs/1_Getting_started/Try_it/4_Using_forms_for_user_input/mysol_1/ch01gs_04/src/app/cart/cart.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { CartService } from '../service/cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.scss']
+})
+export class CartComponent {
+
+  items = this.cartService.getItems();
+
+  constructor(
+    private cartService: CartService
+  ) { }
+
+}
