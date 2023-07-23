c8284b424e8319aee2eb24fb326de0ec7d250e6e
specter01wj <specter01wj@gmail.com>
2023-07-23 13:54:56 -0400

    v3

--- Docs/1_Getting_started/Try_it/3_Managing_data/mysol_1/ch01gs_04/src/app/service/cart.service.ts
+++ Docs/1_Getting_started/Try_it/3_Managing_data/mysol_1/ch01gs_04/src/app/service/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { Product } from '../products';
 
@@ -8,7 +9,7 @@ import { Product } from '../products';
 export class CartService {
   items: Product[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   addToCart(product: Product) {
     this.items.push(product);
