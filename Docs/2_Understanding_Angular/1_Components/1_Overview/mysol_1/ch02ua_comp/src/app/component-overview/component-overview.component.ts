c340531316a75c6f481dd060ea388a446cf718ed
specter01wj <specter01wj@gmail.com>
2023-07-25 23:55:47 -0400

    v2

---
+++ Docs/2_Understanding_Angular/1_Components/1_Overview/mysol_1/ch02ua_comp/src/app/component-overview/component-overview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-overview',
+  templateUrl: './component-overview.component.html',
+  styleUrls: ['./component-overview.component.scss']
+})
+export class ComponentOverviewComponent {
+
+}
