6c40932e308a29f7578ff7250ddc4da877494d7a
specter01wj <specter01wj@gmail.com>
2023-10-07 18:37:05 -0400

    v13

---
+++ Docs/2_Understanding_Angular/1_Components/1_Overview/mysol_1/ch02ua_comp/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ComponentOverviewComponent } from './component-overview/component-overview.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ComponentOverviewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
